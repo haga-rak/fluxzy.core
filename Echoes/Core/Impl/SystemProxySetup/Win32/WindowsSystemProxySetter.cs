756d1c6494d4ab998f91a5639a9e6a36f098fae2
Haga Rakotoharivelo <haga@2befficient.fr>
2022-02-14 18:27:00 +0100

    Force Buiding

---
+++ Echoes/Core/Impl/SystemProxySetup/Win32/WindowsSystemProxySetter.cs
@@ -0,0 +1,17 @@
+﻿namespace Echoes.Core.SystemProxySetup.Win32
+{
+    internal class WindowsSystemProxySetter : ISystemProxySetter
+    {
+        public void ApplySetting(ProxySetting value)
+        {
+            WindowsProxyHelper.SetProxySetting(value);
+        }
+
+        public ProxySetting ReadSetting()
+        {
+            return WindowsProxyHelper.GetSetting();
+        }
+    }
+
+
+}
