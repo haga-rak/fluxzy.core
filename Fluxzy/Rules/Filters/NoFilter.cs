516ab9c2acbdec8bd248a6fda2997e5500d938d7
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-08-01 19:56:01 +0200

    Added DNS mock action

---
+++ Fluxzy/Rules/Filters/NoFilter.cs
@@ -0,0 +1,13 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+namespace Fluxzy.Rules.Filters;
+
+public class NoFilter : Filter
+{
+    protected override bool InternalApply(IAuthority authority, IExchange exchange)
+    {
+        return false; 
+    }
+
+    public override FilterScope FilterScope => FilterScope.OnAuthorityReceived;
+}
