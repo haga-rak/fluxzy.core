ae824eaa1bb010712b7546ddeaf5a26bd4119a24
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-08-02 17:19:28 +0200

    Remote extra header fiels from request headers

---
+++ Fluxzy.Tests/Rules/SimpleRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxzy.Tests.Rules
+{
+    public class SimpleRules
+    {
+        public async Task FilterPerHostAddHeader()
+        {
+
+        } 
+    }
+}
