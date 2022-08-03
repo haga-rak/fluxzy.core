dee8cf9fd1225fa3c834927a12a893952861be7d
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-08-03 12:34:49 +0200

    Added Rule Update and Delete Response Header

---
+++ Fluxzy/Rules/Actions/DeleteResponseHeaderAction.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+using System.Threading.Tasks;
+using Fluxzy.Clients;
+using Fluxzy.Rules.Filters;
+
+namespace Fluxzy.Rules.Actions;
+
+public class DeleteResponseHeaderAction : IAction
+{
+    public DeleteResponseHeaderAction(string headerName)
+    {
+        HeaderName = headerName;
+    }
+
+    public string HeaderName { get; set;  }
+
+    public FilterScope ActionScope => FilterScope.ResponseHeaderReceivedFromRemote;
+
+    public Task Alter(ExchangeContext context, Exchange exchange, Connection connection)
+    {
+        exchange.Response.Header.AltDeleteHeader(HeaderName);
+        return Task.CompletedTask;
+    }
+}
