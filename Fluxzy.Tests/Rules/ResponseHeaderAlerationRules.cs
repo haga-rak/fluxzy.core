64e55493d5a6627e7f7103fe9424d415e654c175
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-08-03 12:34:49 +0200

    Added Rule Update and Delete Response Header

--- Fluxzy.Tests/Rules/ResponseHeaderAlerationRules.cs
+++ Fluxzy.Tests/Rules/ResponseHeaderAlerationRules.cs
@@ -59,5 +59,128 @@ namespace Fluxzy.Tests.Rules
 
             await proxy.WaitUntilDone();
         }
+        
+
+        [Theory]
+        [InlineData(TestConstants.Http11Host)]
+        [InlineData(TestConstants.Http2Host)]
+        public async Task UpdateResponseHeaderWithFilterHostOnly(string host)
+        {
+            var headerValue = "fluxzy-proxy";
+            var headerName = "Server";
+
+            using var proxy = new AddHocConfigurableProxy(PortProvider.Next(), 1, 10);
+
+            using var clientHandler = new HttpClientHandler
+            {
+                Proxy = new WebProxy($"http://{proxy.BindHost}:{proxy.BindPort}"),
+            };
+
+            proxy.StartupSetting.AlterationRules.Add(
+                new Rule(
+                    new UpdateResponseHeaderAction(
+                        headerName, headerValue),
+                    new HostFilter("sandbox.smartizy.com")));
+
+            proxy.Run();
+
+            using var httpClient = new HttpClient(clientHandler);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"{host}/global-health-check");
+
+            using var response = await httpClient.SendAsync(requestMessage);
+            
+           var headerIsPresent =
+                response.Headers.TryGetValues(headerName, out var actualResponseHeaders); 
+
+            Assert.True(headerIsPresent);
+            Assert.NotNull(actualResponseHeaders);
+            Assert.Single(actualResponseHeaders);
+            Assert.Equal(headerValue, actualResponseHeaders.First());
+
+            await proxy.WaitUntilDone();
+        }
+
+        [Theory]
+        [InlineData(TestConstants.Http11Host)]
+        [InlineData(TestConstants.Http2Host)]
+        public async Task UpdateResponseHeaderReuseExistingValueWithFilterHostOnly(string host)
+        {
+            var headerValue = "{{previous}}fluxzy-proxy";
+            var headerName = "Server";
+
+            using var proxy = new AddHocConfigurableProxy(PortProvider.Next(), 1, 10);
+
+            using var clientHandler = new HttpClientHandler
+            {
+                Proxy = new WebProxy($"http://{proxy.BindHost}:{proxy.BindPort}"),
+            };
+
+            proxy.StartupSetting.AlterationRules.Add(
+                new Rule(
+                    new UpdateResponseHeaderAction(
+                        headerName, headerValue),
+                    new HostFilter("sandbox.smartizy.com")));
+
+            proxy.Run();
+
+            using var httpClient = new HttpClient(clientHandler);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"{host}/global-health-check");
+
+            using var response = await httpClient.SendAsync(requestMessage);
+            
+           var headerIsPresent =
+                response.Headers.TryGetValues(headerName, out var actualResponseHeaders); 
+
+            Assert.True(headerIsPresent);
+            Assert.NotNull(actualResponseHeaders);
+            Assert.Single(actualResponseHeaders);
+            Assert.EndsWith("fluxzy-proxy", actualResponseHeaders.First());
+
+            await proxy.WaitUntilDone();
+        }
+
+
+        [Theory]
+        [InlineData(TestConstants.Http11Host)]
+        [InlineData(TestConstants.Http2Host)]
+        public async Task DeleteResponseHeaderWithFilterHostOnly(string host)
+        {
+            var headerName = "server";
+
+            using var proxy = new AddHocConfigurableProxy(PortProvider.Next(), 1, 10);
+
+            using var clientHandler = new HttpClientHandler
+            {
+                Proxy = new WebProxy($"http://{proxy.BindHost}:{proxy.BindPort}"),
+            };
+
+            proxy.StartupSetting.AlterationRules.Add(
+                new Rule(
+                    new DeleteResponseHeaderAction(
+                        headerName),
+                    new HostFilter("sandbox.smartizy.com")));
+
+            proxy.Run();
+
+            using var httpClient = new HttpClient(clientHandler);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"{host}/global-health-check");
+
+            using var response = await httpClient.SendAsync(requestMessage);
+
+            var checkResult = await response.GetCheckResult();
+
+            var headerIsPresent =
+                response.Headers.TryGetValues(headerName, out var actualResponseHeaders);
+
+            Assert.False(headerIsPresent);
+
+            await proxy.WaitUntilDone();
+        }
     }
 }
---
+++ Fluxzy/Rules/Actions/UpdateResponseHeaderAction.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+using System.Threading.Tasks;
+using Fluxzy.Clients;
+using Fluxzy.Rules.Filters;
+
+namespace Fluxzy.Rules.Actions
+{
+    public class UpdateResponseHeaderAction : IAction
+    {
+        public UpdateResponseHeaderAction(string headerName, string headerValue)
+        {
+            HeaderName = headerName;
+            HeaderValue = headerValue;
+        }
+
+        public string HeaderName { get; set;  }
+
+        public string HeaderValue { get; set;  }
+
+        public FilterScope ActionScope => FilterScope.ResponseHeaderReceivedFromRemote;
+
+        public Task Alter(ExchangeContext context, Exchange exchange, Connection connection)
+        {
+            exchange.Response.Header.AltReplaceHeaders(
+                HeaderName, HeaderValue);
+
+            return Task.CompletedTask;
+        }
+    }
+}
