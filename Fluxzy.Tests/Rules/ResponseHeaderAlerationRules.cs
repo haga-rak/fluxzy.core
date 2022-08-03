ae7e3104d33b1bdd2afca7de9514c89bd2b9c6b2
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-08-03 12:11:33 +0200

    Add Rule Add Response Header

---
+++ Fluxzy.Tests/Rules/ResponseHeaderAlerationRules.cs
@@ -0,0 +1,63 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fluxzy.Rules;
+using Fluxzy.Rules.Actions;
+using Fluxzy.Rules.Filters.RequestFilters;
+using Fluxzy.Tests.Tools;
+using Fluxzy.Tests.Utils;
+using Xunit;
+
+namespace Fluxzy.Tests.Rules
+{
+    public class ResponseHeaderAlterationRules
+    {
+
+        [Theory]
+        [InlineData(TestConstants.Http11Host)]
+        [InlineData(TestConstants.Http2Host)]
+        public async Task AddNewResponseHeaderWithFilterHostOnly(string host)
+        {
+            var headerValue = "value!!!";
+            var headerName = "X-pRepend-Header";
+
+            using var proxy = new AddHocConfigurableProxy(PortProvider.Next(), 1, 10);
+
+            using var clientHandler = new HttpClientHandler
+            {
+                Proxy = new WebProxy($"http://{proxy.BindHost}:{proxy.BindPort}"),
+            };
+
+            proxy.StartupSetting.AlterationRules.Add(
+                new Rule(
+                    new AddResponseHeaderAction(
+                        headerName, headerValue),
+                    new HostFilter("sandbox.smartizy.com")));
+
+            proxy.Run();
+
+            using var httpClient = new HttpClient(clientHandler);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"{host}/global-health-check");
+
+            using var response = await httpClient.SendAsync(requestMessage);
+
+            var checkResult = await response.GetCheckResult();
+
+            var headerIsPresent =
+                response.Headers.TryGetValues(headerName, out var actualResponseHeaders); 
+
+            Assert.True(headerIsPresent);
+            Assert.NotNull(actualResponseHeaders);
+            Assert.Single(actualResponseHeaders);
+            Assert.Equal(headerValue, actualResponseHeaders.First());
+
+            await proxy.WaitUntilDone();
+        }
+    }
+}
