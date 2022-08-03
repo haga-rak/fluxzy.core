5d2649ddcaea6bf828b76cf647568f8fcc784a64
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-08-03 11:23:50 +0200

    Added Rule Update Header Reuse existing value

--- Fluxzy.Tests/Rules/SimpleRules.cs
+++ Fluxzy.Tests/Rules/SimpleRules.cs
@@ -105,5 +105,53 @@ namespace Fluxzy.Tests.Rules
 
             await proxy.WaitUntilDone();
         }
+
+        [Theory]
+        [InlineData(TestConstants.Http11Host)]
+        [InlineData(TestConstants.Http2Host)]  
+        public async Task UpdateRequestHeaderReuseExistingValueWithFilterHostOnly(string host)
+        {
+            var headerName = "x-h";
+            var headerValue = "Cd";
+            var headerNewValue = "{{previous}} Ab";
+            var headerValueAltered = "Cd Ab"; 
+
+            using var proxy = new AddHocConfigurableProxy(PortProvider.Next(), 1, 10);
+
+            using var clientHandler = new HttpClientHandler
+            {
+                Proxy = new WebProxy($"http://{proxy.BindHost}:{proxy.BindPort}"),
+            };
+
+            proxy.StartupSetting.AlterationRules.Add(
+                new Rule(
+                    new UpdateRequestHeaderAction(
+                        headerName, headerNewValue),
+                    new HostFilter("sandbox.smartizy.com")));
+
+            proxy.Run();
+
+            using var httpClient = new HttpClient(clientHandler);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"{host}/global-health-check");
+
+            requestMessage.Headers.Add(headerName, headerValue);
+
+            using var response = await httpClient.SendAsync(requestMessage);
+            
+            var checkResult = await response.GetCheckResult();
+
+            var matchingHeaders = checkResult.
+                Headers?.Where(h => 
+                    h.Name.Equals(headerName, StringComparison.OrdinalIgnoreCase) 
+                    && h.Value == headerValueAltered)
+                .ToList();
+
+            Assert.NotNull(matchingHeaders);
+            Assert.Single(matchingHeaders);
+
+            await proxy.WaitUntilDone();
+        }
     }
 }
