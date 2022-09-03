3411ab0364a427ab67e57f913977b3f40bc329a7
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-09-03 23:36:16 +0200

    fix : correct duplicate id when postion = 0

--- Fluxzy.Desktop.Ui/ClientApp/src/app/services/ui.service.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/services/ui.service.ts
@@ -1,73 +1,109 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { Observable, of, Subject,tap,take, map, filter,switchMap, combineLatest } from 'rxjs';
-import { ExchangeBrowsingState, ExchangeState, FileState, UiState } from '../core/models/auto-generated';
+import {
+    Observable,
+    of,
+    Subject,
+    tap,
+    take,
+    map,
+    filter,
+    switchMap,
+    combineLatest,
+    distinct,
+} from 'rxjs';
+import {
+    ExchangeBrowsingState,
+    ExchangeState,
+    FileState,
+    UiState,
+} from '../core/models/auto-generated';
 import { MenuService } from '../core/services/menu-service.service';
 import { ApiService } from './api.service';
-import { ExchangeSelectedIds, ExchangeSelectionService } from './exchange-selection.service';
+import { ExchangeContentService } from './exchange-content.service';
+import {
+    ExchangeSelectedIds,
+    ExchangeSelectionService,
+} from './exchange-selection.service';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class UiStateService {
+    private uiState$: Subject<UiState> = new Subject<UiState>();
 
-    private uiState$ : Subject<UiState> = new Subject<UiState>() ; 
-    
-    constructor(private httpClient: HttpClient, private menuService : MenuService, private apiService : ApiService, private selectionService : ExchangeSelectionService) { 
-        this.refreshUiState() ; 
-        
-        this.apiService.registerEvent('uiUpdate', (state : UiState) => {
+    constructor(
+        private httpClient: HttpClient,
+        private menuService: MenuService,
+        private apiService: ApiService,
+        private selectionService: ExchangeSelectionService,
+        private exchangeContentService:  ExchangeContentService
+    ) {
+        this.refreshUiState();
+
+        this.apiService.registerEvent('uiUpdate', (state: UiState) => {
             this.uiState$.next(state);
         });
 
-
-        combineLatest(
-            [
-                this.getUiState(),
-                this.selectionService.getCurrentSelectedIds()
-            ]
-        )
+        combineLatest([
+            this.getUiState(),
+            this.selectionService.getCurrentSelectedIds(),
+        ])
             .pipe(
-           // tap(t => console.log(t)),
-            tap(t => {
-                const uiState = t[0] ;
-                const selection = t[1] ; 
-                
-                this.menuService.updateMenu(uiState,selection.length);
-            })).subscribe();
+                tap((t) => {
+                    const uiState = t[0];
+                    const selection = t[1];
+
+                    this.menuService.updateMenu(uiState, selection.length);
+                })
+            )
+        .subscribe();
+
+        this.getFileState()
+                .pipe(
+                    map(f => f.workingDirectory),
+                    distinct(), 
+                    switchMap(f => this.apiService.readTrunkState(f)),
+                    tap(t => this.exchangeContentService.update(t))
+                )
+                .subscribe();
     }
 
-    private refreshUiState() : void {
-
-        this.httpClient.get<UiState>(`api/ui/state`)
+    private refreshUiState(): void {
+        this.httpClient
+            .get<UiState>(`api/ui/state`)
             .pipe(
-                tap(t => this.uiState$.next(t)),
+                tap((t) => this.uiState$.next(t)),
                 take(1)
-            ).subscribe();
+            )
+            .subscribe();
 
-        // Open file 
-        this.menuService.getNextOpenFile()
-                .pipe(
-                    filter(t => !!t), 
-                    switchMap(fileName => this.apiService.fileOpen(fileName)),
-                 //   tap(t => this.uiState$.next(t))
-                ).subscribe() ; 
+        // Open file
+        this.menuService
+            .getNextOpenFile()
+            .pipe(
+                filter((t) => !!t),
+                switchMap((fileName) => this.apiService.fileOpen(fileName))
+                //   tap(t => this.uiState$.next(t))
+            )
+            .subscribe();
 
         // New file
-        this.menuService.getNextOpenFile()
-                .pipe(
-                    filter(t => t === ''), // new file 
-                    switchMap(fileName => this.apiService.fileNew()),
-                   // tap(t => this.uiState$.next(t))
-                ).subscribe() ; 
+        this.menuService
+            .getNextOpenFile()
+            .pipe(
+                filter((t) => t === ''), // new file
+                switchMap((fileName) => this.apiService.fileNew())
+                // tap(t => this.uiState$.next(t))
+            )
+            .subscribe();
     }
 
-    public getUiState() : Observable<UiState> {
-        return this.uiState$.asObservable() ; 
+    public getUiState(): Observable<UiState> {
+        return this.uiState$.asObservable();
     }
 
-    public getFileState() : Observable<FileState> {
-        return this.uiState$.asObservable().pipe(map(u => u.fileState )) ; 
+    public getFileState(): Observable<FileState> {
+        return this.uiState$.asObservable().pipe(map((u) => u.fileState));
     }
-    
 }
