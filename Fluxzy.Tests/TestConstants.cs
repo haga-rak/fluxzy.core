058d0d5787a57e29c0836065964c2e235c87024f
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-09-25 15:05:07 +0200

    update : improve test memberdata readability

--- Fluxzy.Tests/TestConstants.cs
+++ Fluxzy.Tests/TestConstants.cs
@@ -1,5 +1,7 @@
 // Copyright © 2022 Haga Rakotoharivelo
 
+using System;
+
 namespace Fluxzy.Tests
 {
     public static class TestConstants
@@ -8,5 +10,20 @@ namespace Fluxzy.Tests
         public const string Http2Host = "https://sandbox.smartizy.com:5001";
         public const string Http2Host2 = "https://sandbox.smartizy.com:4430";
         public const string WssHost = "wss://sandbox.smartizy.com:5001";
+
+
+        public static string GetHost(string protocol)
+        {
+            if (protocol == "http11")
+                return Http11Host;
+
+            if (protocol == "http2")
+                return Http2Host;
+
+            throw new ArgumentException(nameof(protocol), "Unknown protocol"); 
+        }
     }
+
+
+    
 }
