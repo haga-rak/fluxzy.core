d9ae3ba51a9480c721e0b83d47e667d626fbf4e3
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-09-27 10:42:05 +0200

    dev : add two formatter producer

---
+++ Fluxzy/Formatters/Producers/AuthorizationBearerProducer.cs
@@ -0,0 +1,46 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+using System;
+using System.Linq;
+using Fluxzy.Readers;
+using Fluxzy.Screeners;
+
+namespace Fluxzy.Formatters.Producers
+{
+    public class AuthorizationBearerProducer : IFormattingProducer<AuthorizationBearerFormattingResult>
+    {
+        public string ResultTitle => " \"Bearer token\"";
+
+        public AuthorizationBearerFormattingResult? Build(ExchangeInfo exchangeInfo, IArchiveReader archiveReader)
+        {
+            var headers = exchangeInfo.GetRequestHeaders()?.ToList();
+
+            if (headers == null)
+                return null;
+
+            var targetHeader =
+                headers.FirstOrDefault(h =>
+                    h.Name.Span.Equals("Authorization", StringComparison.OrdinalIgnoreCase)
+                    && h.Value.Span.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase));
+
+            if (targetHeader == null)
+                return null;
+
+            var token = targetHeader.Value.Span.Slice("Bearer ".Length).ToString();
+
+            return new AuthorizationBearerFormattingResult(
+               ResultTitle, token);
+        }
+    }
+
+    public class AuthorizationBearerFormattingResult : FormattingResult
+    {
+        public string Token { get; }
+
+        public AuthorizationBearerFormattingResult(string title, string token)
+            : base(title)
+        {
+            Token = token;
+        }
+    }
+}
