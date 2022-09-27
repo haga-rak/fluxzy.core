907c33e89f2f41b6b7e4c3234c4a49d9f838d5b6
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-09-27 10:42:05 +0200

    dev : add two formatter producer

---
+++ Fluxzy/Formatters/FormattingResult.cs
@@ -0,0 +1,17 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+namespace Fluxzy.Screeners
+{
+    public abstract class FormattingResult
+    {
+        protected FormattingResult(string title)
+        {
+            Title = title;
+        }
+
+        public string Title {  get; }
+
+        public string ? ErrorMessage { get; set; }
+        
+    }
+}
---
+++ Fluxzy/Formatters/IFormattingProducer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fluxzy.Readers;
+
+namespace Fluxzy.Screeners
+{
+    internal interface IFormattingProducer<out T>
+        where T : FormattingResult
+    {
+        string ResultTitle { get; }
+
+        T? Build(ExchangeInfo exchangeInfo, IArchiveReader archiveReader); 
+    }
+}
