2596b5c84c5c62f9924306d7ad7d97beb22f490a
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-01 17:13:21 +0200

    feat : add request cookie

---
+++ Fluxzy/Formatters/Producers/RequestCookieProducer.cs
@@ -0,0 +1,77 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Fluxzy.Readers;
+using Fluxzy.Screeners;
+
+namespace Fluxzy.Formatters.Producers
+{
+    public class RequestCookieProducer : IFormattingProducer<RequestCookieResult>
+    {
+        public string ResultTitle => "Cookies";
+
+        public RequestCookieResult? Build(ExchangeInfo exchangeInfo, IArchiveReader archiveReader)
+        {
+            var headers = exchangeInfo.GetRequestHeaders()?.ToList();
+
+            if (headers == null)
+                return null;
+
+            var targetHeaders =
+                headers.Where(h =>
+                    h.Name.Span.Equals("Cookie", StringComparison.OrdinalIgnoreCase));
+
+            var requestCookies = new List<RequestCookie>(); 
+
+            foreach (var headerValue in targetHeaders)
+            {
+                var cookieLines = headerValue.Value.ToString()
+                                             .Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries)
+                                             .Select(s => s.Trim());
+
+                foreach (var cookieLine in cookieLines)
+                {
+                    var cookieNameValueTab = cookieLine.Split('=');
+
+                    if (cookieNameValueTab.Length < 2)
+                        continue;
+
+                    var cookieName = HttpUtility.UrlDecode(cookieNameValueTab[0]);
+                    var cookieValue = HttpUtility.UrlDecode(string.Join("=", cookieNameValueTab.Skip(1)));
+
+                    requestCookies.Add(new RequestCookie(cookieName, cookieValue)); 
+                }
+            }
+
+            return requestCookies.Any() ? new RequestCookieResult(ResultTitle, requestCookies) : null; 
+
+        }
+    }
+
+    public class RequestCookieResult : FormattingResult
+    {
+        public RequestCookieResult(string title, List<RequestCookie> cookies) : base(title)
+        {
+            Cookies = cookies;
+        }
+
+        public List<RequestCookie> Cookies { get; }
+    }
+
+
+    public class RequestCookie
+    {
+        public RequestCookie(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; }
+
+        public string Value { get; }
+    }
+}
