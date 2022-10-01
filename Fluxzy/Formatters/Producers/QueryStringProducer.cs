7f201e084a7f9b1fc0d2eedd032d3a145eb36885
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-01 17:13:21 +0200

    feat : add request cookie

--- Fluxzy/Formatters/Producers/QueryStringProducer.cs
+++ Fluxzy/Formatters/Producers/QueryStringProducer.cs
@@ -4,14 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Fluxzy.Clients;
 using Fluxzy.Readers;
 using Fluxzy.Screeners;
 
 namespace Fluxzy.Formatters.Producers
 {
+
     public class QueryStringProducer : IFormattingProducer<QueryStringResult>
     {
-        public string ResultTitle => throw new NotImplementedException();
+        public string ResultTitle => "Query string";
 
         public QueryStringResult? Build(ExchangeInfo exchangeInfo, IArchiveReader archiveReader)
         {
@@ -21,20 +23,19 @@ namespace Fluxzy.Formatters.Producers
                 return null;
 
             var res = HttpUtility.ParseQueryString(uri.Query);
-            
 
             var items = res.AllKeys.SelectMany(k => res.GetValues((string)k)?.Select(v => new QueryStringItem(k, v)))
                            .Where(t => t != null)
                            .ToList();
 
-            return !items.Any() ? null : new QueryStringResult(items);
+            return !items.Any() ? null : new QueryStringResult(ResultTitle, items);
         }
     }
 
 
     public class QueryStringResult : FormattingResult
     {
-        public QueryStringResult(List<QueryStringItem> items) : base("Query string")
+        public QueryStringResult(string title, List<QueryStringItem> items) : base(title)
         {
             Items = items;
         }
