4c6002c0d81797ee9c7d92d9ee11b9940b0d0fc6
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-08 00:39:33 +0200

    update : move code generator to custom components

--- Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-viewer/details-response/response-json-result/response-json-result.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-viewer/details-response/response-json-result/response-json-result.component.ts
@@ -17,11 +17,13 @@ export class ResponseJsonResultComponent implements OnInit {
     @Input() public context: ExchangeContextInfo;
 
     public alreadyFormatted : boolean = false; 
+    public title: string ; 
 
     constructor(private systemCallService : SystemCallService) {}
 
     ngOnInit(): void {
       this.alreadyFormatted = this.context.responseBodyText === this.model.formattedContent; 
+      this.title = !this.alreadyFormatted ? 'This JSON has been formatted to provide better readability.' : ''; 
     }
     
     public setClipboard(text : string) : void {
