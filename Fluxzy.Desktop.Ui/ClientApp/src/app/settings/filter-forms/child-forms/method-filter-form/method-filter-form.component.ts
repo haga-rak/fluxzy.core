5cd7c1b38ede982bae001768cb0c2fba4a90a6f4
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-12 11:52:27 +0200

    dev : method filters

--- Fluxzy.Desktop.Ui/ClientApp/src/app/settings/filter-forms/child-forms/method-filter-form/method-filter-form.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/settings/filter-forms/child-forms/method-filter-form/method-filter-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Filter, MethodFilter } from '../../../../core/models/auto-generated';
 
 @Component({
@@ -8,6 +9,7 @@ import { Filter, MethodFilter } from '../../../../core/models/auto-generated';
 })
 export class MethodFilterFormComponent implements OnInit {
     @Input() rawFilter : Filter ; 
+    @Input() validateRequest : Observable<boolean> ; 
 
     public filter : MethodFilter ; 
 
