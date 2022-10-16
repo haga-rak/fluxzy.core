a73d342327975c40d96610432a121ddaac9364fa
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-17 01:52:36 +0200

    update : add error toolbar filter

--- Fluxzy.Desktop.Services/Models/ToolBarFilter.cs
+++ Fluxzy.Desktop.Services/Models/ToolBarFilter.cs
@@ -6,14 +6,17 @@ namespace Fluxzy.Desktop.Services.Models
 {
     public class ToolBarFilter
     {
-        public ToolBarFilter(string shortName, Filter filter)
+        public ToolBarFilter(string shortName, Filter filter, string? description = null)
         {
             ShortName = shortName;
             Filter = filter;
+            Description = description;
         }
 
         public string ShortName { get; }
 
         public Filter Filter { get;  }
+
+        public string?  Description { get; }
     }
 }
