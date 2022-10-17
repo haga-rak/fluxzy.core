2717a98ad521b3af3d4e920a5c595c9f26f2a61d
Haga Rakotoharivelo <haga@2befficient.fr>
2022-10-17 10:17:30 +0200

    chore : move models to Models directory

---
+++ Fluxzy.Desktop.Services/Models/FilteredExchangeState.cs
@@ -0,0 +1,41 @@
+﻿// Copyright © 2022 Haga Rakotoharivelo
+
+namespace Fluxzy.Desktop.Services.Models
+{
+    public class FilteredExchangeState : IEquatable<FilteredExchangeState>
+    {
+        public bool Equals(FilteredExchangeState? other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return Exchanges.SetEquals(other.Exchanges);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+
+            return Equals((FilteredExchangeState)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Exchanges.GetHashCode();
+        }
+
+        public FilteredExchangeState(IEnumerable<int> exchanges)
+        {
+            Exchanges = new(exchanges);
+        }
+
+        public HashSet<int> Exchanges { get; }
+    }
+}
