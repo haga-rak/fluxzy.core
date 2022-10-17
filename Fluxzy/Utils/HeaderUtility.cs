78c064bd2a14fd4b685b71c4a40d2a1688cc7106
Haga Rakotoharivelo <haga@2befficient.fr>
2022-10-17 14:50:28 +0200

    dev : add subdomain

--- Fluxzy/Utils/HeaderUtility.cs
+++ Fluxzy/Utils/HeaderUtility.cs
@@ -91,4 +91,22 @@ namespace Fluxzy.Utils
             return null;
         }
     }
+
+
+    public static class SubdomainUtility
+    {
+        public static bool TryGetSubDomain(string host, out string? subDomain)
+        {
+            var splittedHost = host.Split(new[] {"."}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (splittedHost.Length > 2) {
+                subDomain = string.Join(".", splittedHost.Skip(1));
+                return true;
+            }
+
+            subDomain = null;
+            return false;
+
+        }
+    }
 }
