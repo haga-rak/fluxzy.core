5c83b36cdbe98cf9a09ac34c73b5bbc4b745af77
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-20 01:07:11 +0200

    dev : add rsBuffer

---
+++ Fluxzy/Misc/ResizableBuffers/RsBuffer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluxzy.Misc.ResizableBuffers
+{
+    public class RsBuffer : IDisposable
+    {
+        private RsBuffer(byte[] buffer)
+        {
+            Buffer = buffer;
+        }
+
+        public byte [] Buffer { get; private set;  }
+
+        public static RsBuffer Allocate(int size)
+        {
+            var rawBuffer = ArrayPool<byte>.Shared.Rent(size);
+
+            var result = new RsBuffer(rawBuffer);
+
+            return result; 
+        }
+
+        public void Multiply(int size)
+        {
+            var newBuffer = ArrayPool<byte>.Shared.Rent(Buffer.Length * size);
+            System.Buffer.BlockCopy(Buffer, 0, newBuffer, 0, Buffer.Length);
+            ArrayPool<byte>.Shared.Return(Buffer);
+            Buffer = newBuffer;
+        }
+
+        public void Extend(int extensionLength)
+        {
+            var newBuffer = ArrayPool<byte>.Shared.Rent(Buffer.Length + extensionLength);
+            System.Buffer.BlockCopy(Buffer, 0, newBuffer, 0, Buffer.Length);
+            ArrayPool<byte>.Shared.Return(Buffer);
+            Buffer = newBuffer;
+        }
+
+        public void Dispose()
+        {
+            ArrayPool<byte>.Shared.Return(Buffer);
+        }
+    }
+}
