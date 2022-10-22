93a979545273e883fe5dafdaeceaa5090a208aa4
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-10-22 17:22:57 +0200

    dev : search feature on precreate filter

--- Fluxzy.Desktop.Ui/ClientApp/src/app/core/models/filter-constants.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/core/models/filter-constants.ts
@@ -16,7 +16,6 @@ export const CheckRegexValidity = (input : string) : boolean  => {
     return true;
 }
 
-
 export const RequestHeaderNames : string[] = ["A-IM", "Accept", "Accept-Charset", "Accept-Datetime", "Accept-Encoding", "Accept-Language", "Access-Control-Request-Method,",
     "Access-Control-Request-Headers", "Authorization", "Cache-Control", "Connection", "Content-Encoding", "Content-Length", "Content-MD5", "Content-Type",
     "Cookie", "Date", "Expect", "Forwarded", "From", "Host", "HTTP2-Settings", "If-Match", "If-Modified-Since", "If-None-Match", "If-Range", "If-Unmodified-Since",
@@ -24,8 +23,6 @@ export const RequestHeaderNames : string[] = ["A-IM", "Accept", "Accept-Charset"
     "Via", "Warning", "Upgrade-Insecure-Requests", "X-Requested-With", "DNT", "X-Forwarded-For", "X-Forwarded-Host", "X-Forwarded-Proto", "Front-End-Https",
     "X-Http-Method-Override", "X-ATT-DeviceId", "X-Wap-Profile", "Proxy-Connection", "X-UIDH", "X-Csrf-Token", "X-Request-ID,", "X-Correlation-ID,", "Save-Data",
 ];
-
-
 export const ResponseHeaderNames : string [] = ["Accept-CH", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Access-Control-Expose-Headers",
     "Access-Control-Max-Age", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Accept-Patch", "Accept-Ranges", "Age", "Allow", "Alt-Svc",
     "Cache-Control", "Connection", "Content-Disposition", "Content-Encoding", "Content-Language", "Content-Length", "Content-Location", "Content-MD5",
@@ -34,3 +31,17 @@ export const ResponseHeaderNames : string [] = ["Accept-CH", "Access-Control-All
     "Upgrade", "Vary", "Via", "Warning", "WWW-Authenticate", "X-Frame-Options", "Content-Security-Policy", "X-Content-Security-Policy", "X-WebKit-CSP",
     "Expect-CT", "NEL", "Permissions-Policy", "Refresh", "Report-To", "Status", "Timing-Allow-Origin", "X-Content-Duration", "X-Content-Type-Options",
     "X-Powered-By", "X-Redirect-By", "X-Request-ID", "X-Correlation-ID", "X-UA-Compatible", "X-XSS-Protection",];
+
+
+export const SearchByKeyword = (input : string, q : string) : boolean  => {
+    const qArray = q.split(" ") ;
+
+    input = input.toLocaleLowerCase() ;
+
+    for (const qItem of qArray){
+        if (input.indexOf(qItem) < 0)
+            return false;
+    }
+
+    return true;
+}
