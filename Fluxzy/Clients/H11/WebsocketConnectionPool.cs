d00cb3e50c807ba1520e85b6ef66fc00e5b79d90
Haga Rakotoharivelo <haga@2befficient.fr>
2022-10-24 18:03:15 +0200

    dev : checkpoint

--- Fluxzy/Clients/H11/WebsocketConnectionPool.cs
+++ Fluxzy/Clients/H11/WebsocketConnectionPool.cs
@@ -2,17 +2,22 @@
 // 
 
 using System;
+using System.Buffers;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Pipelines;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Fluxzy.Clients.H2.Encoder.Utils;
+using Fluxzy.Misc;
 using Fluxzy.Misc.ResizableBuffers;
 using Fluxzy.Misc.Streams;
 using Fluxzy.Writers;
+using ICSharpCode.SharpZipLib.Tar;
 
 namespace Fluxzy.Clients.H11
 {
@@ -217,16 +222,208 @@ namespace Fluxzy.Clients.H11
     }
 
 
-
-
-
-    public class WsBlock
+    /// <summary>
+    /// A websocket reading stream 
+    /// </summary>
+    public class WebSocketStream
     {
+        private readonly Pipe _pipe;
 
+        public WebSocketStream(int limitFile = )
+        {
+            _pipe = new Pipe();
+            
+        }
+
+        private async Task InitRead()
+        {
+            while (true)
+            {
+                if (!_pipe.Reader.TryRead(out var readResult)) {
+                    readResult = await _pipe.Reader.ReadAsync();
+                }
+
+                if (readResult.IsCompleted || readResult.IsCanceled)
+                    return;
+
+                var buffer = readResult.Buffer;
+                var headerLength = -1;
+
+                WsFrame wsFrame = default; 
+
+                if (!((headerLength = TryReadWsFrameHeader(ref buffer, ref wsFrame)) < 0)) {
+
+                    // not enough data to complete the header frame send back to read 
+
+                    _pipe.Reader.AdvanceTo(buffer.Start); 
+                    continue; 
+                }
+
+                _pipe.Reader.AdvanceTo(buffer.GetPosition(headerLength));
+
+                
+
+            }
+        }
+
+
+        private int TryReadWsFrameHeader(ref ReadOnlySequence<byte> sequencBuffer, ref WsFrame wsFrame)
+        {
+            if (sequencBuffer.Length < 2)
+                return -1;
+
+            var buffer = sequencBuffer.FirstSpan;
+
+            wsFrame.FinalFragment = (buffer[0] & 0x80) > 0;
+
+            wsFrame.OpCode = (WsOpCode) (buffer[0] & 0xF);
+
+            int byteIndex = 1;
+
+            var maskedPayload = (buffer[byteIndex] & 0x80) > 0;
+            var payloadIndicator = (byte) (buffer[byteIndex] & 0X7f);
+
+            if (payloadIndicator < 126) {
+                wsFrame.PayloadLength = payloadIndicator;
+            }
+            else
+            {
+                var startBuffer = buffer.Slice(byteIndex);
+
+                if (payloadIndicator == 126) {
+                    if (startBuffer.Length < 2)
+                        return -1; // Not enough data 
+
+                    wsFrame.PayloadLength = BinaryPrimitives.ReadUInt16BigEndian(startBuffer);
+                    byteIndex += 2; 
+                }
+                else
+                {
+                    if (startBuffer.Length < 4)
+                        return -1; // Not enough data 
+
+                    wsFrame.PayloadLength = BinaryPrimitives.ReadInt64BigEndian(startBuffer);
+                    byteIndex += 8;
+                }
+
+                byteIndex += 1;
+            }
+
+
+            if (maskedPayload) {
+
+                var startBuffer = buffer.Slice(byteIndex);
+
+                if (startBuffer.Length < 4)
+                    return -1;
+
+                wsFrame.PayloadLength = BinaryPrimitives.ReadInt32BigEndian(startBuffer);
+                byteIndex += 4;
+            }
+
+            // Reading the buffer 
+
+            return byteIndex; 
+        }
+
+
+        private void Write(ReadOnlySpan<byte> data)
+        {
+            _pipe.Writer.WriteAsync();
+        }
     }
+
+    public struct WsFrame
+    {
+        public int FrameId { get; set; }
+
+        public long PayloadLength { get; set; }
+
+        public WsOpCode OpCode { get; set; }
+
+        public bool FinalFragment { get; set; }
+
+        public byte []  Data { get; set; }
+    }
+    
 
     public class WsMessage
     {
+        public WsMessage(int id)
+        {
+            Id = id; 
+        }
 
+        public int Id { get; set; }
+
+        public WsOpCode OpCode { get; set; }
+
+        public long CurrentLength { get; set; }
+
+        public byte[]?  Data { get; set; }
+
+        internal async Task AddFrame(
+            WsFrame wsFrame, int maxWsMessageLengthBuffered, PipeReader pipeReader,
+                            Func<Stream> outStream)
+        {
+            if (wsFrame.OpCode != 0) {
+                OpCode = wsFrame.OpCode; 
+            }
+
+            if (wsFrame.FinalFragment && CurrentLength == 0 && wsFrame.PayloadLength < maxWsMessageLengthBuffered) {
+                // Build direct buffer for message 
+
+                var readResult = await pipeReader.ReadAtLeastAsync((int) wsFrame.PayloadLength);
+
+                Data = new byte[wsFrame.PayloadLength];
+
+                readResult.Buffer.FirstSpan.Slice(0, (int)wsFrame.PayloadLength)
+                          .CopyTo(Data);
+
+                pipeReader.AdvanceTo(readResult.Buffer.GetPosition(wsFrame.PayloadLength));
+            }
+            else {
+                
+
+
+
+            }
+                
+            CurrentLength += wsFrame.PayloadLength; 
+        }
+
+
+
+
+    }
+
+
+
+    //*  %x0 denotes a continuation frame
+
+    //    *  %x1 denotes a text frame
+
+    //    *  %x2 denotes a binary frame
+
+    //    *  %x3-7 are reserved for further non-control frames
+
+    //    *  %x8 denotes a connection close
+
+    //    *  %x9 denotes a ping
+
+    //    *  %xA denotes a pong
+
+    //    *  %xB-F are reserved for further control frames
+
+
+    [Flags]
+    public enum WsOpCode
+    {
+        Continuation = 0, 
+        Text = 1 ,
+        Binary = 2, 
+        ConnectionClose = 8, 
+        Ping = 9, 
+        Pong = 0xA,
     }
 }
