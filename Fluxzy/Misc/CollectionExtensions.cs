1f5d7a076a054dd0a2760390eb763b53a32d1a88
Haga Rakotoharivelo <haga@2befficient.fr>
2022-10-28 15:47:46 +0200

    feat : add GlobalApplyTag for n-n relation exchange tags

---
+++ Fluxzy/Misc/CollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿// // Copyright 2022 - Haga Rakotoharivelo
+// 
+
+using System.Collections.Generic;
+
+namespace Fluxzy.Misc
+{
+    public static class CollectionExtensions
+    {
+        public static void AddRange<T>(this HashSet<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items) {
+
+                collection.Add(item);
+            }
+        }
+    }
+}
