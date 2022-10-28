7232c59b9bca31ffd3b0985e06415f3463986360
Haga Rakotoharivelo <haga@2befficient.fr>
2022-10-28 13:37:30 +0200

    feat : retrive ws body content end to end done

--- Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-viewer/details-response/ws-message-formatting-result/ws-message-formatting-result.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-viewer/details-response/ws-message-formatting-result/ws-message-formatting-result.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ExchangeContextInfo, ExchangeInfo, ResponseTextContentResult, WsMessageFormattingResult} from "../../../../core/models/auto-generated";
+import {ExchangeContextInfo, ExchangeInfo, ResponseTextContentResult, WsMessage, WsMessageFormattingResult} from "../../../../core/models/auto-generated";
 import {SystemCallService} from "../../../../core/services/system-call.service";
+import {filter, switchMap, take, tap} from "rxjs";
+import { ApiService } from '../../../../services/api.service';
+import {StatusBarService} from "../../../../services/status-bar.service";
 
 @Component({
   selector: 'app-ws-message-formatting-result',
@@ -12,11 +15,24 @@ export class WsMessageFormattingResultComponent  implements OnInit {
     @Input('formatter') public model: WsMessageFormattingResult;
     @Input() public context: ExchangeContextInfo;
 
-    constructor(private systemCallService : SystemCallService) {}
+    constructor(private systemCallService : SystemCallService, private apiService : ApiService, private statusBarService : StatusBarService) {}
 
     ngOnInit(): void {}
 
     public setClipboard(text : string) : void {
         this.systemCallService.setClipBoard(text);
+    }
+
+    public saveWebSocketBody(message : WsMessage) : void {
+        const suggestedName = `ws-${this.exchange.id}-${message.id}-${message.direction}.data`;
+
+        this.systemCallService.requestFileOpen(suggestedName)
+            .pipe(
+                take(1),
+                filter(f => !!f),
+                switchMap(f => this.apiService.exchangeSaveWebSocketBody(this.exchange.id,
+                    message.id, f, message.direction)),
+                tap(_ => this.statusBarService.addMessage('Content saved') )
+            ).subscribe() ;
     }
 }
