5821265514cde0a5951d44cf612e0f9cd34e7885
Haga Rakotoharivelo <haga@2befficient.fr>
2022-11-09 19:05:09 +0100

    core feat : add CA verification

---
+++ Fluxzy/Misc/IpUtils/X509CustomExtensions.cs
@@ -0,0 +1,18 @@
+﻿// // Copyright 2022 - Haga Rakotoharivelo
+// 
+
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Fluxzy.Misc.IpUtils
+{
+    public static class X509CustomExtensions
+    {
+        public static bool IsCa(this X509Certificate2 certificate)
+        {
+            return certificate.Extensions
+                              .OfType<X509BasicConstraintsExtension>()
+                              .Any(t => t.CertificateAuthority);
+        }
+    }
+}
