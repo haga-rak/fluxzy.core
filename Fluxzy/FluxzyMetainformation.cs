1af37ba706078a00ec3b485956160f4f9b81e67b
Haga Rakotoharivelo <haga@2befficient.fr>
2022-11-16 18:39:38 +0100

    core feat : add assembly meta information provider

---
+++ Fluxzy/FluxzyMetainformation.cs
@@ -0,0 +1,24 @@
+﻿// // Copyright 2022 - Haga Rakotoharivelo
+// 
+
+using System.Reflection;
+
+namespace Fluxzy
+{
+    public static class FluxzyMetainformation
+    {
+        private static string? _version;
+
+        public static string? Version
+        {
+            get
+            {
+                if (_version != null)
+                    return _version;
+
+                return _version = typeof(Proxy).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                               .InformationalVersion;
+            }
+        }
+    }
+}
