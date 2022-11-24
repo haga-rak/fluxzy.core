1992dc27cedb454461ced543b8bddd2b16b6b280
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-11-25 00:17:41 +0100

    test : add unit test for export

--- Fluxzy.Tests/Archiving/HttpArchive.cs
+++ Fluxzy.Tests/Archiving/HttpArchive.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Fluxzy.Tests.Archiving.Fixtures;
 using Xunit;
@@ -15,15 +16,67 @@ namespace Fluxzy.Tests.Archiving
         }
 
         [Fact]
-        public void VerifyBaseContent()
+        public void CheckMainHarEntryProperties()
         {
             var document = _fixture.Document.RootElement;
             var log = document.GetProperty("log");
             var entries = log.GetProperty("entries").EnumerateArray().ToList();
-            var exchanges = _fixture.Exchanges; 
-            
+            var exchanges = _fixture.Exchanges;
+
+            var str = document.ToString();
+
             Assert.Equal("1.2", log.GetProperty("version").GetString());
-            Assert.Equal(4, exchanges.Count);
+            Assert.Equal(exchanges.Count, entries.Count);
+            
+            foreach (var exchange in exchanges)
+            {
+                var entry = entries.FirstOrDefault(e => e.GetProperty("_exchangeId")
+                                                         .GetInt32() == exchange.Id); 
+
+                Assert.NotEqual(default, entry);
+                
+                Assert.Equal(exchange.RequestHeader.Method.ToString(), 
+                    entry.GetProperty("request")
+                   .GetProperty("method")
+                   .GetString());
+                
+                Assert.Equal(exchange.RequestHeader.GetFullUrl(),
+                    entry.GetProperty("request")
+                         .GetProperty("url")
+                         .GetString());
+
+                foreach (var requestHeader in exchange.RequestHeader.Headers)
+                {
+                    var header = entry.GetProperty("request")
+                                      .GetProperty("headers")
+                                      .EnumerateArray()
+                                      .FirstOrDefault(h => h.GetProperty("name")
+                                                            .GetString() == requestHeader.Name.ToString()
+
+                                      && h.GetProperty("value")
+                                          .GetString() == requestHeader.Value.ToString());
+
+                    Assert.NotEqual(default, header);
+                }
+
+                if (exchange.ResponseHeader != null)
+                {
+                    foreach (var responseHeader in exchange.ResponseHeader.Headers)
+                    {
+                        var header = entry.GetProperty("response")
+                                          .GetProperty("headers")
+                                          .EnumerateArray()
+                                          .FirstOrDefault(h => h.GetProperty("name")
+                                                                .GetString() == responseHeader.Name.ToString()
+
+                                          && h.GetProperty("value")
+                                              .GetString() == responseHeader.Value.ToString());
+
+                        Assert.NotEqual(default, header);
+                    }
+
+                }
+            }
             
         }
 
