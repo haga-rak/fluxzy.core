a8839034017a36726e0859f4682259fb1ef19658
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-11-29 12:55:10 +0100

    ui : better ui for search filter

--- Fluxzy.Desktop.Ui/ClientApp/src/app/menu/menu.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/menu/menu.component.ts
@@ -1,29 +1,24 @@
-import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
-import { ElectronService } from '../core/services';
+import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
+import {ElectronService} from '../core/services';
 
-@Component({ 
+@Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-   
+    public menuVisibility: any = {};
 
-    public menuVisibility : any = {} ; 
-
-    
-    constructor(private eRef: ElementRef) { 
+    constructor(private eRef: ElementRef) {
 
     }
-
-    
     @HostListener('document:click', ['$event'])
-    clickout(event) : void{
-      if(!this.eRef.nativeElement.contains(event.target)) {
-         this.menuVisibility = {} ; 
-      }
+    clickout(event): void {
+        if (!this.eRef.nativeElement.contains(event.target)) {
+            this.menuVisibility = {};
+        }
     }
-    
+
     ngOnInit(): void {
 
     }
