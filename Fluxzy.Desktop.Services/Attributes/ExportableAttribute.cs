146ad3beebc0e944f29613cdd4f3886176d92e3e
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-12-03 00:31:33 +0100

    update : add global ExportableAttribute

---
+++ Fluxzy.Desktop.Services/Attributes/ExportableAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxzy.Desktop.Services.Attributes
+{
+    /// <summary>
+    /// Marks a class as exportable to typescript
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ExportableAttribute : Attribute
+    {
+        
+    }
+}
