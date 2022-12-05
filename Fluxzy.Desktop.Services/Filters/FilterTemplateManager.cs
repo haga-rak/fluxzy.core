eff37bccaf92d660c02bc23b3ba21ef37231d463
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-12-05 23:16:33 +0100

    update : FilterTemplateManager now load filters by controlled reflection

--- Fluxzy.Desktop.Services/Filters/FilterTemplateManager.cs
+++ Fluxzy.Desktop.Services/Filters/FilterTemplateManager.cs
@@ -1,59 +1,86 @@
-﻿using Fluxzy.Rules.Filters;
-using Fluxzy.Rules.Filters.RequestFilters;
-using Fluxzy.Rules.Filters.ResponseFilters;
-using Fluxzy.Rules.Filters.ViewOnlyFilters;
+﻿using System.Reflection;
+using Fluxzy.Rules.Filters;
 
 namespace Fluxzy.Desktop.Services.Filters
 {
     public class FilterTemplateManager
     {
+        private static readonly List<TypeFilter> TypeFilters;
+        private static readonly List<Filter> DefaultTemplates = new();
+        private static readonly Dictionary<Type, Filter> Instances = new(); 
+
+        static FilterTemplateManager()
+        {
+            TypeFilters = typeof(Filter).Assembly.GetTypes()
+                                     .Where(derivedType => typeof(Filter).IsAssignableFrom(derivedType)
+                                                           && derivedType.IsClass
+                                                           && !derivedType.IsAbstract)
+                                     .Where(derivedType => derivedType.GetCustomAttribute<FilterMetaDataAttribute>() != null)
+                                     // TODO : update this suboptimal double call of GetCustomAttribute
+                                     .Select(derivedType => new TypeFilter(derivedType, derivedType.GetCustomAttribute<FilterMetaDataAttribute>()!))
+                                     .ToList();
+
+            foreach (var item in TypeFilters)
+            {
+                var filter = CreateFilter(item);
+                Instances[item.Type] = filter;
+                DefaultTemplates.Add(filter);
+            }
+        }
+
+        private static Filter CreateFilter(TypeFilter item)
+        {
+            var constructor = item.Type.GetConstructors()
+                                  .OrderByDescending(t => t.GetParameters().Length).First();
+
+            var arguments = new List<object>();
+
+            foreach (var argument in constructor.GetParameters())
+            {
+                arguments.Add(argument.ParameterType == typeof(string)
+                    ? string.Empty
+                    : ReflectionHelper.GetDefault(argument.ParameterType));
+            }
+
+            var filter = (Filter) constructor.Invoke(arguments.ToArray());
+
+            return filter;
+        }
+
         // TODO load this by reflection
         public List<FilterTemplate> ReadAvailableTemplates()
         {
-            var res = new List<FilterTemplate>
-            {
-                new FullUrlFilter(string.Empty),
-                new HostFilter(string.Empty),
-                new MethodFilter(string.Empty),
-                new PathFilter(string.Empty),
-                new RequestHeaderFilter("Header_name", "Header_value"),
-
-                new PostFilter(),
-                new PatchFilter(),
-                new PutFilter(),
-                new DeleteFilter(),
-
-                new ContentTypeJsonFilter(),
-                new ContentTypeXmlFilter(),
-                new ImageFilter(),
-                new ResponseHeaderFilter("Header_name", "Header_value"),
-                new StatusCodeSuccessFilter(),
-                new StatusCodeClientErrorFilter(),
-                new StatusCodeServerErrorFilter(),
-                new StatusCodeFilter(),
-                new StatusCodeRedirectionFilter(),
-
-                new AnyFilter(),
-                new FilterCollection
-                {
-                    Common = true
-                },
-                new IpEgressFilter("164.132.227.11"),
-                new H11TrafficOnlyFilter(),
-                new H2TrafficOnlyFilter(),
-                new IsWebSocketFilter(),
-                new HasRequestBodyFilter(),
-
-                new HasCommentFilter(),
-                new HasTagFilter(),
-                new CommentSearchFilter(string.Empty),
-                new TagContainsFilter(null),
-                new AgentLabelFilter(""),
-                new SearchTextFilter(""),
-                new NetworkErrorFilter()
-            };
+            var res = DefaultTemplates.Select(f => new FilterTemplate(f)).ToList();
 
             return res;
         }
+
+        class TypeFilter
+        {
+            public TypeFilter(Type type, FilterMetaDataAttribute metaData)
+            {
+                Type = type;
+                MetaData = metaData;
+            }
+
+            public Type Type { get; }
+
+            public FilterMetaDataAttribute MetaData { get; }
+        }
+    }
+    
+    internal static class ReflectionHelper
+    {
+        public static object GetDefault(Type t)
+        {
+            return typeof(ReflectionHelper).GetMethod(nameof(GetDefaultGeneric))!
+                                        .MakeGenericMethod(t).Invoke(null, null)!;
+        }
+
+        private static T? GetDefaultGeneric<T>()
+        {
+            return default(T);
+        }
+
     }
 }
