44ec0819be4521b08b90449e381fbb193af1b14a
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-12-05 23:53:18 +0100

    update : adding filter description endpoint. Closing #42

--- Fluxzy/Rules/Filters/FilterScope.cs
+++ Fluxzy/Rules/Filters/FilterScope.cs
@@ -6,7 +6,7 @@ namespace Fluxzy.Rules.Filters
     /// The filter scope defines the minimal timing where a filter can be run.
     /// These specific timing are :
     ///  - OnAuthorityReceived : The moment when fluxzy knows the destination authority. It will occur the moment where fluxzy parsed
-    /// CONNECT request in a typical proxy communication
+    /// CONNECT request in a regular proxy communication
     ///  - RequestHeaderReceivedFromClient : The moment when the full request header is parsed
     ///  - RequestBodyReceivedFromClient :  This timing is trigger only after the request body is sent to the upstream due to streaming
     ///  - ResponseHeaderReceivedFromRemote : The moment where fluxzy got the response header from the server
@@ -20,7 +20,6 @@ namespace Fluxzy.Rules.Filters
         RequestBodyReceivedFromClient,
         ResponseHeaderReceivedFromRemote,
         ResponseBodyReceivedFromRemote,
-
         OutOfScope = 99999
     }
 }
