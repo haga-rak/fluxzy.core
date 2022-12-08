a18a79f1b37a1979b2e859590da5b3d6414035e7
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2022-12-08 18:22:28 +0100

    update : cleanup packager output

--- Fluxzy.Desktop.Ui/ClientApp/forge.config.js
+++ Fluxzy.Desktop.Ui/ClientApp/forge.config.js
@@ -1,22 +1,42 @@
 module.exports = {
-  packagerConfig: {},
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+    packagerConfig: {
+        ignore: [
+            "node_modules",
+            "\\.angular",
+            "\\.idea",
+            "\\.run",
+            "\\.vscode",
+            "\\.npmrc",
+            "^run$",
+            "^e2e$",
+            "/src/",
+            "\\.gitignore",
+            "\\.editorconfig",
+            "forge.config.js",
+            "[.](cmd|user|DotSettings|csproj|sln)$"
+        ],
+        win32metadata: {
+            CompanyName: "Smartizy",
+            ProductName: "Fluxzy",
+        }
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+    ],
 };
