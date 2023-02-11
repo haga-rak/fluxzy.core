52ec318f50119052c8b79dfdabd0ca41ea319555
haga.rakotoharivelo@gmail.com <haga.rakotoharivelo@gmail.com>
2023-02-11 22:48:54 +0100

    update : add wait message while loading backend

--- Fluxzy.Desktop.Ui/ClientApp/src/app/home/home.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuItemConstructorOptions } from 'electron';
 import { filter, tap } from 'rxjs';
-import { ExchangeInfo } from '../core/models/auto-generated';
+import {ExchangeInfo, UiState} from '../core/models/auto-generated';
 import { GlobalMenuItems } from '../core/models/menu-models';
 import { ElectronService } from '../core/services';
 import { MenuService } from '../core/services/menu-service.service';
@@ -20,17 +20,26 @@ import { UiStateService } from '../services/ui.service';
 export class HomeComponent implements OnInit {
 
     public selectedExchange: ExchangeInfo;
+    public uiState: UiState | null = null;
 
     constructor(private router: Router,
         private menuService : MenuService,
         private exchangeSelectionService : ExchangeSelectionService,
         private dialogService : DialogService,
+        private uiStateService : UiStateService,
         private cdr: ChangeDetectorRef) { }
 
     ngOnInit(): void {
 
+        this.uiStateService.getUiState()
+            .pipe(
+                tap(t => this.uiState = t),
+                tap(t => this.cdr.detectChanges()),
+            ).subscribe()
+
         this.menuService.init();
         this.dialogService.init();
+
 
         this.exchangeSelectionService.getSelected()
             .pipe(
