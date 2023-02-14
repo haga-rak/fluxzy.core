b7456209b26c5e0fb6f9201c0cde04d4d51415cc
Haga Rakotoharivelo <haga@2befficient.fr>
2023-02-14 20:02:01 +0100

    test : unit test setup for curl testing, breakage on directory deletion. More assert on request body too.

--- Fluxzy.Tests/CurlExportTests.cs
+++ Fluxzy.Tests/CurlExportTests.cs
@@ -5,78 +5,167 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using Fluxzy.Clients.H2.Encoder.Utils;
 using Fluxzy.Misc.Streams;
 using Fluxzy.Readers;
 using Fluxzy.Tests.Cli.Scaffolding;
 using Fluxzy.Tests.Common;
+using Fluxzy.Utils.Curl;
 using Xunit;
 
 namespace Fluxzy.Tests
 {
     public class CurlExportTests
     {
-        [Fact]
-        public async Task Run_Cli_Output()
+        [Theory]
+        [InlineData("GET", TestPayloadType.FlatText)]
+        [InlineData("POST", TestPayloadType.FlatText)]
+        [InlineData("POST", TestPayloadType.FormContentEncoded)]
+        public async Task Run_And_Check_Curl_Response_Against_HttpClient(string methodString, 
+            TestPayloadType payloadType)
+        {
+            var converter = new CurlRequestConverter();
+            
+            var rootDir = Guid.NewGuid().ToString();
+            var directoryName = $"{rootDir}/httpclient-test";
+
+            try {
+
+                var quickTestResult = await DoRequestThroughHttpClient(directoryName, 
+                    new HttpMethod(methodString), payloadType);
+                
+                using var archiveReader = quickTestResult.ArchiveReader;
+
+                var curlDirectoryOutput = $"{rootDir}/curl-test"; 
+
+                var commandLine = "start -l 127.0.0.1/0";
+                commandLine += $" -d {curlDirectoryOutput}";
+
+                var commandLineHost = new FluxzyCommandLineHost(commandLine);
+
+                await using (var fluxzyInstance = await commandLineHost.Run()) {
+                    var commandResult = converter.BuildCurlRequest(archiveReader, quickTestResult.ExchangeInfo, new CurlProxyConfiguration(
+                        "127.0.0.1", fluxzyInstance.ListenPort));
+
+                    var res = CurlUtility.RunCurl(commandResult.FlatCommandLineWithProxyArgs,
+                        out var stdout, out var stderr);
+                }
+
+                using var curlArchiveReader = new DirectoryArchiveReader(curlDirectoryOutput);
+
+                var exchanges = curlArchiveReader.ReadAllExchanges().ToList();
+                
+                var httpClientExchange = quickTestResult.ExchangeInfo;
+                var curlExchange = exchanges.FirstOrDefault()!;
+
+                Assert.NotNull(curlExchange);
+
+                var curlFlatHeader = string.Join("\r\n",
+                    curlExchange
+                        .GetRequestHeaders()
+                        .OrderBy(r => r.Name.ToString()).Select(s => $"{s.Name} : {s.Value}"));
+                
+                var httpClientFlatHeader = string.Join("\r\n",
+                    httpClientExchange
+                        .GetRequestHeaders()
+                        .OrderBy(r => r.Name.ToString()).Select(s => $"{s.Name} : {s.Value}"));
+
+                Assert.Equal(httpClientFlatHeader, curlFlatHeader);
+            }
+            finally {
+                if (Directory.Exists(rootDir))
+                    Directory.Delete(rootDir, true);
+            }
+        }
+
+        private static async Task<QuickTestResult> DoRequestThroughHttpClient(
+            string directoryName, HttpMethod method, TestPayloadType payloadType)
         {
             // Arrange 
-            var protocol = "http2"; 
+            var protocol = "http2";
 
-            var rootDir = Guid.NewGuid().ToString();
-            var directoryName = $"{rootDir}/curl-test";
-            var fileName = $"{rootDir}/curl-test.fxzy";
 
             var commandLine = "start -l 127.0.0.1/0";
-
             commandLine += $" -d {directoryName}";
-            
+
             var commandLineHost = new FluxzyCommandLineHost(commandLine);
 
-            await using (var fluxzyInstance = await commandLineHost.Run())
-            {
+            await using (var fluxzyInstance = await commandLineHost.Run()) {
                 using var proxiedHttpClient = new ProxiedHttpClient(fluxzyInstance.ListenPort);
 
-                var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                var requestMessage = new HttpRequestMessage(method,
                     $"{TestConstants.GetHost(protocol)}/global-health-check");
-                
+
+                if (method == HttpMethod.Post) {
+                    
+                    if (payloadType == TestPayloadType.FlatText) {
+                        requestMessage.Content = new StringContent("Some flatString", Encoding.UTF8); 
+                    }
+
+                    if (payloadType == TestPayloadType.FormContentEncoded) {
+                        requestMessage.Content = new FormUrlEncodedContent(new Dictionary<string, string>() {
+                            {"A", "B"},
+                            {"C", "p"},
+                            {"d", "g"},
+                            {"f", "r"},
+                        }); 
+                    }
+                }
+
+                requestMessage.Headers.Add("x-header-test", "123456");
+
                 // Act 
                 using var response = await proxiedHttpClient.Client.SendAsync(requestMessage);
 
                 await response.Content.ReadAsStringAsync();
-
-                // Assert
-                await AssertionHelper.ValidateCheck(requestMessage, null, response);
+                
             }
 
             // Assert outputDirectory content
 
-            using (IArchiveReader archiveReader = new DirectoryArchiveReader(directoryName))
-            {
-                var exchanges = archiveReader.ReadAllExchanges().ToList();
-                var connections = archiveReader.ReadAllConnections().ToList();
+            IArchiveReader archiveReader = new DirectoryArchiveReader(directoryName);
 
-                var exchange = exchanges.FirstOrDefault()!;
+            var exchanges = archiveReader.ReadAllExchanges().ToList();
+            var connections = archiveReader.ReadAllConnections().ToList();
 
-                var connection = connections.First();
+            var exchange = exchanges.FirstOrDefault()!;
 
-                Assert.NotNull(exchange);
-                Assert.Equal(0, await commandLineHost.ExitCode);
-                Assert.Single(exchanges);
-                Assert.Single(connections);
+            var connection = connections.First();
 
-                Assert.Equal(200, exchange.StatusCode);
-                Assert.Equal(connection.Id, exchange.ConnectionId);
-            }
+            var quickTestResult = new QuickTestResult(archiveReader, exchange);
 
-            if (Directory.Exists(directoryName))
-                Directory.Delete(directoryName, true);
+            Assert.NotNull(exchange);
+            Assert.Equal(0, await commandLineHost.ExitCode);
+            Assert.Single(exchanges);
+            Assert.Single(connections);
 
-            if (Directory.Exists(rootDir))
-                Directory.Delete(rootDir, true);
+            Assert.Equal(200, exchange.StatusCode);
+            Assert.Equal(connection.Id, exchange.ConnectionId);
+            return quickTestResult;
+        }
+    }
 
-            if (File.Exists(fileName))
-                File.Delete(fileName);
+    public enum TestPayloadType
+    {
+        FlatText = 1 ,
+        FormContentEncoded = 2, 
+    }
+    
+    internal class QuickTestResult
+    {
+        public QuickTestResult(IArchiveReader archiveReader, ExchangeInfo exchangeInfo)
+        {
+            ArchiveReader = archiveReader;
+            ExchangeInfo = exchangeInfo;
         }
 
+        public IArchiveReader ArchiveReader { get; }
+
+        public ExchangeInfo ExchangeInfo { get;  }
+
+        
     }
 }
---
+++ Fluxzy/Utils/Curl/CurlUtility.cs
@@ -0,0 +1,55 @@
+﻿// // Copyright 2022 - Haga Rakotoharivelo
+// 
+
+using System.Diagnostics;
+
+namespace Fluxzy.Utils.Curl
+{
+    public static class CurlUtility
+    {
+        public static bool CheckCurlIsInstalled()
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "curl",
+                    Arguments = "--version",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+
+            return process.ExitCode == 0;
+        }
+
+        public static bool RunCurl(string args, out string stdout, out string stdErr)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "curl",
+                    Arguments = args,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                }
+            };
+
+            
+            process.Start();
+
+            stdout = process.StandardOutput.ReadToEnd();
+            stdErr = process.StandardError.ReadToEnd();
+            
+            process.WaitForExit();
+
+            return process.ExitCode == 0;
+        }
+    }
+}
