629d9ba06412c05674eace1f406964ebce546feb
haga <haga.rakotoharivelo@gmail.com>
2023-02-21 01:33:02 +0100

    test : fix unit test bad path

--- Fluxzy.Tests/Certificates/CertificateManager.cs
+++ Fluxzy.Tests/Certificates/CertificateManager.cs
@@ -1,6 +1,8 @@
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using Fluxzy.Tests._Files;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using Xunit;
 
 namespace Fluxzy.Tests.Certificates
@@ -12,7 +14,8 @@ namespace Fluxzy.Tests.Certificates
 
         public CertificateManager()
         {
-            _certificate = new X509Certificate2(StorageContext.fluxzytest);
+            
+            _certificate = new X509Certificate2("_Files/Certificates/fluxzytest.txt");
             _manager = new DefaultCertificateAuthorityManager();
         }
         
