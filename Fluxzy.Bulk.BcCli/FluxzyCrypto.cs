0e98fdfb95d2cf545633df714dec8a30e5f8cf7f
Haga Rakotoharivelo <haga@2befficient.fr>
2023-02-24 16:26:48 +0100

    update : POC OK pour TLS 1.1

---
+++ Fluxzy.Bulk.BcCli/FluxzyCrypto.cs
@@ -0,0 +1,40 @@
+﻿// // Copyright 2022 - Haga Rakotoharivelo
+// 
+
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Tls;
+using Org.BouncyCastle.Tls.Crypto;
+using Org.BouncyCastle.Tls.Crypto.Impl.BC;
+
+namespace Fluxzy.Bulk.BcCli
+{
+    class FluxzyCrypto : BcTlsCrypto
+    {
+        public FluxzyCrypto(SecureRandom sr)
+            : base(sr)
+        {
+            
+        }
+
+        public override TlsSecret GenerateRsaPreMasterSecret(ProtocolVersion version)
+        {
+            var res =  base.GenerateRsaPreMasterSecret(version);
+            return res; 
+        }
+        
+
+        public override TlsSecret AdoptSecret(TlsSecret secret)
+        {
+            var resultSecret =  base.AdoptSecret(secret);
+
+            // byte[] data = new byte[1024];
+            var data = secret.Extract();
+            MasterSecret = new byte[data.Length];
+            data.CopyTo(MasterSecret, 0);
+            
+            return resultSecret; 
+        }
+
+        public byte[]?  MasterSecret { get; set; }
+    }
+}
