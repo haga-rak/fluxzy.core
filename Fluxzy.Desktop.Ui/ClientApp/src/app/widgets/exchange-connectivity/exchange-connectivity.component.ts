8d82a26a957752238535b7f4096abafef759398a
haga <haga.rakotoharivelo@gmail.com>
2023-03-03 00:37:16 +0100

    fix : ui, blinking connection

--- Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-connectivity/exchange-connectivity.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-connectivity/exchange-connectivity.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {filter, Subject, switchMap, take, tap} from 'rxjs';
-import { ConnectionInfo, ExchangeInfo } from '../../core/models/auto-generated';
-import { ApiService } from '../../services/api.service';
+import {ConnectionInfo, ExchangeInfo} from '../../core/models/auto-generated';
+import {ApiService} from '../../services/api.service';
 import {SystemCallService} from "../../core/services/system-call.service";
 import {StatusBarService} from "../../services/status-bar.service";
 
@@ -12,38 +12,44 @@ import {StatusBarService} from "../../services/status-bar.service";
 })
 export class ExchangeConnectivityComponent implements OnInit, OnChanges {
 
-    public connection : ConnectionInfo | null = null ;
+    public connection: ConnectionInfo | null = null;
 
     @Input() public exchange: ExchangeInfo | null;
-    @Input() public connectionId : number ;
+    @Input() public connectionId: number;
 
-    constructor(private apiService : ApiService, private systemCallService : SystemCallService, private statusBarService : StatusBarService) {}
+    constructor(private apiService: ApiService, private systemCallService: SystemCallService, private statusBarService: StatusBarService) {
+    }
 
     ngOnInit(): void {
-      this.refresh() ;
-      console.log(this.exchange);
+        this.refresh();
+        console.log(this.exchange);
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-      this.refresh() ;
+        this.refresh();
     }
 
-    private refresh() : void {
-      this.connection = null ;
+    private refresh(): void {
 
-      this.apiService.connectionGet(this.connectionId)
-        .pipe(
-          tap(
-            t => this.connection = t
-          ),
-          tap(
-            t => console.log(t)
-          )
-        ).subscribe() ;
+        if (this.connection?.id === this.connectionId){
+            return ; // no need to refresh
+        }
+
+        this.connection = null;
+
+        this.apiService.connectionGet(this.connectionId)
+            .pipe(
+                tap(
+                    t => this.connection = t
+                ),
+                tap(
+                    t => console.log(t)
+                )
+            ).subscribe();
     }
 
-    public downloadRawCapture() : void {
-        this.systemCallService.requestFileSave( `connection-${this.exchange.connectionId}.cap`)
+    public downloadRawCapture(): void {
+        this.systemCallService.requestFileSave(`connection-${this.exchange.connectionId}.cap`)
             .pipe(
                 take(1),
                 filter(t => !!t),
@@ -52,11 +58,11 @@ export class ExchangeConnectivityComponent implements OnInit, OnChanges {
             ).subscribe();
     }
 
-    public openRawCapture() : void {
+    public openRawCapture(): void {
         this.apiService.connectionOpenRawCapture(this.exchange.connectionId)
             .pipe(
                 take(1),
-                filter(t =>  !t),
+                filter(t => !t),
                 tap(_ => this.statusBarService.addMessage("Raw capture opening failed"))
             ).subscribe();
     }
