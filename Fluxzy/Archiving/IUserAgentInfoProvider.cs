f4ca49eaa0b70fc3b5c10139c4f5ff730389607d
haga <haga.rakotoharivelo@gmail.com>
2023-03-04 03:13:55 +0100

    packaging : fix packaging error (NETSDK1152)

--- Fluxzy/Archiving/IUserAgentInfoProvider.cs
+++ Fluxzy/Archiving/IUserAgentInfoProvider.cs
@@ -1,17 +1,9 @@
-﻿// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
 namespace Fluxzy
 {
     public interface IUserAgentInfoProvider
     {
         string GetFriendlyName(int id, string rawUserAgentValue);
-    }
-
-    public class DefaultUserAgentInfoProvider : IUserAgentInfoProvider
-    {
-        public string GetFriendlyName(int id, string rawUserAgentValue)
-        {
-            return rawUserAgentValue;
-        }
     }
 }
