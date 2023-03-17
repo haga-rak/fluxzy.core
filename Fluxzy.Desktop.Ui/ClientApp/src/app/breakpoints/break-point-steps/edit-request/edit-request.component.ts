e6879cc60c1d757c3cc9336de1504581988902b4
Haga Rakotoharivelo <haga@2befficient.fr>
2023-03-17 19:10:05 +0100

    ui : add connection setup step component

---
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/breakpoints/break-point-steps/edit-request/edit-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-request',
+  templateUrl: './edit-request.component.html',
+  styleUrls: ['./edit-request.component.scss']
+})
+export class EditRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
