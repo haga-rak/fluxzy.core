e82c6d72d4aa81251cb86a65b546426b0eee86d6
haga <haga.rakotoharivelo@gmail.com>
2023-03-21 20:54:29 +0100

    update : add edit or add header

---
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/shared/header-editor/dialogs/add-header/add-or-edit-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-header',
+  templateUrl: './add-header.component.html',
+  styleUrls: ['./add-header.component.scss']
+})
+export class AddHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
