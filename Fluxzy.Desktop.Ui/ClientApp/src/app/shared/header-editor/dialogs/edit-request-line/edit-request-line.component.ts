d2e3715f488ba006a3cc14246c1660d413e3e244
haga <haga.rakotoharivelo@gmail.com>
2023-03-23 14:25:53 +0100

    ui : add EditRequestLine and EditResponseLine forms

---
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/shared/header-editor/dialogs/edit-request-line/edit-request-line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-request-line',
+  templateUrl: './edit-request-line.component.html',
+  styleUrls: ['./edit-request-line.component.scss']
+})
+export class EditRequestLineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
