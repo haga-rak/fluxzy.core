3f77478761dcc4847f63ce0b106a5fd9b8d9ce60
Haga Rakotoharivelo <haga@2befficient.fr>
2023-04-06 12:53:37 +0200

    test : reduce load on test server

--- Fluxzy.Tests/ProxyTests.cs
+++ Fluxzy.Tests/ProxyTests.cs
@@ -45,7 +45,7 @@ namespace Fluxzy.Tests
 
         private static async Task Receiving_Multiple_Repeating_Header_Value_Call(HttpClient httpClient)
         {
-            var repeatCount = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 50 : 10;
+            var repeatCount = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 20 : 10;
 
             var hosts = new[] {
                 TestConstants.Http2Host,
