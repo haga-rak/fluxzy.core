1f972fa5fea5ff49505fef197c73e98368844499
haga <haga.rakotoharivelo@gmail.com>
2023-04-13 22:28:34 +0200

    fix : Body Summary, skip content-type when it's absent

--- Fluxzy/Formatters/Producers/Responses/ResponseBodySummaryProducer.cs
+++ Fluxzy/Formatters/Producers/Responses/ResponseBodySummaryProducer.cs
@@ -1,4 +1,4 @@
-// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+﻿// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
 using System;
 using Fluxzy.Extensions;
@@ -24,7 +24,9 @@ namespace Fluxzy.Formatters.Producers.Responses
                 !string.IsNullOrWhiteSpace(uri.LocalPath))
                 preferredFileName = uri.LocalPath;
 
-            return new ResponseBodySummaryResult($"{ResultTitle} ({exchangeInfo.ContentType})", context.ResponseBodyLength.Value,
+            return new ResponseBodySummaryResult(
+                string.IsNullOrWhiteSpace(exchangeInfo.ContentType) ?  $"{ResultTitle}" : 
+                $"{ResultTitle} ({exchangeInfo.ContentType})", context.ResponseBodyLength.Value,
                 context.CompressionInfo.CompressionName!, exchangeInfo.GetResponseHeaderValue("content-type"),
                 context.ResponseBodyText, preferredFileName);
         }
