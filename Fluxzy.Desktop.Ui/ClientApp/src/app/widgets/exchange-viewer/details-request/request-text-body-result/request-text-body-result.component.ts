c07d4071e52405504e9e39b07ff027db986b5555
haga <haga.rakotoharivelo@gmail.com>
2023-04-16 02:06:23 +0200

    Show commit dialog when clicking on Exchange table

--- Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-viewer/details-request/request-text-body-result/request-text-body-result.component.ts
+++ Fluxzy.Desktop.Ui/ClientApp/src/app/widgets/exchange-viewer/details-request/request-text-body-result/request-text-body-result.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { RequestTextBodyResult } from '../../../../core/models/auto-generated';
+import {Component, Input, OnInit} from '@angular/core';
+import {RequestTextBodyResult} from '../../../../core/models/auto-generated';
 
 @Component({
-  selector: 'app-request-text-body-result',
-  templateUrl: './request-text-body-result.component.html',
-  styleUrls: ['./request-text-body-result.component.scss']
+    selector: 'app-request-text-body-result',
+    templateUrl: './request-text-body-result.component.html',
+    styleUrls: ['./request-text-body-result.component.scss']
 })
 export class RequestTextBodyResultComponent implements OnInit {
-  
-  @Input('formatter') public model: RequestTextBodyResult;
 
-  constructor() { }
+    @Input('formatter') public model: RequestTextBodyResult;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
 
 }
