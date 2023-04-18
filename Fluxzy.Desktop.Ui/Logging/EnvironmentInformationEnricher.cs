1c6edfc3eed06318d6f652a2640b089f5eb19670
Haga R <haga.rakotoharivelo@gmail.com>
2023-04-19 01:16:54 +0200

    Add seq remote integration

---
+++ Fluxzy.Desktop.Ui/Logging/EnvironmentInformationEnricher.cs
@@ -0,0 +1,25 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System.Runtime.InteropServices;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Fluxzy.Desktop.Ui.Logging
+{
+    class EnvironmentInformationEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "TargetId", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" 
+                ? $"{Environment.MachineName}/{Environment.UserDomainName}"
+                : UidProvider.Current()));
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "AppVersion", "v0.1.18"));
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "Runtime", RuntimeInformation.RuntimeIdentifier));
+        }
+    }
+}
