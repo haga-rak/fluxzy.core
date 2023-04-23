02bbad2ce22b6a534e2413aa21b016a8e2389c1f
Haga R <haga.rakotoharivelo@gmail.com>
2023-04-23 02:20:44 +0200

    Add generic filter tests

---
+++ src/Fluxzy/Rules/Filters/RequestFilters/HasAnyCookieOnRequestFilter.cs
@@ -0,0 +1,33 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System.Linq;
+using Fluxzy.Clients.H2.Encoder;
+using Fluxzy.Formatters.Producers.Requests;
+
+namespace Fluxzy.Rules.Filters.RequestFilters
+{
+    [FilterMetaData(
+        LongDescription = "Exchange having any request cookie"
+    )]
+    public class HasAnyCookieOnRequestFilter : Filter
+    {
+        public override FilterScope FilterScope  => FilterScope.RequestHeaderReceivedFromClient;
+
+        public override string GenericName => "Cookie";
+
+        public override string ShortName => "Has any cookie";
+
+        public override bool PreMadeFilter => true;
+
+        protected override bool InternalApply(IAuthority authority, IExchange exchange, IFilteringContext filteringContext)
+        {
+            if (exchange == null)
+                return false; 
+
+            var requestCookies =
+                HttpHelper.ReadRequestCookies(exchange.GetRequestHeaders().Select(h => (GenericHeaderField)h));
+
+            return requestCookies.Any();
+        }
+    }
+}
--- test/Fluxzy.Tests/Rules/GlobalFilterTests.cs
+++ test/Fluxzy.Tests/Rules/GlobalFilterTests.cs
@@ -1,5 +1,7 @@
 // Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Fluxzy.Rules.Filters;
@@ -45,5 +47,62 @@ namespace Fluxzy.Tests.Rules
 
             Assert.False(result);
         }
+
+        /// <summary>
+        /// Test multiple filter
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="genericFilterInfo"></param>
+        /// <returns></returns>
+        [Theory]
+        [MemberData(nameof(CheckPass_Generic_ArgBuilder))]
+        public async Task CheckPass_Generic(string host, CheckPassGenericFilterInfo genericFilterInfo)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{host}/global-health-check");
+
+            genericFilterInfo.AlterAction(requestMessage);
+
+            var result = await CheckPass(requestMessage, genericFilterInfo.Filter);
+
+            Assert.Equal(genericFilterInfo.ShouldPass, result);
+        }
+
+        public static IEnumerable<object[]> CheckPass_Generic_ArgBuilder()
+        {
+            var testedHosts = new string[] { TestConstants.Http11Host, TestConstants.Http2Host };
+            
+            var checkPassGenericFilterInfos = new List<CheckPassGenericFilterInfo>() {
+                new(new HasAnyCookieOnRequestFilter(), r => r.Headers.Add("Cookie", "test=1"), true),
+                new(new HasAnyCookieOnRequestFilter(), r =>  {}, false),
+                new(new GetFilter(), r => r.Method = HttpMethod.Get, true),
+                new(new GetFilter(), r => r.Method = HttpMethod.Delete, false),
+                new(new FullUrlFilter("smartizy.com"), r => {}, true),
+                new(new FullUrlFilter("smartzzizy.com"), r => {}, false),
+            };
+
+            foreach (var host in testedHosts) {
+                foreach (var checkPassGenericFilterInfo in checkPassGenericFilterInfos) {
+
+                    yield return new object[] { host, checkPassGenericFilterInfo };
+                }
+            }
+        }
+    }
+
+
+    public class CheckPassGenericFilterInfo
+    {
+        public CheckPassGenericFilterInfo(Filter filter, Action<HttpRequestMessage> alterAction, bool shouldPass)
+        {
+            Filter = filter;
+            AlterAction = alterAction;
+            ShouldPass = shouldPass;
+        }
+
+        public Filter Filter { get; }
+
+        public Action<HttpRequestMessage> AlterAction { get; }
+
+        public bool ShouldPass { get; }
     }
 }
