f882901f5678b8bc94a458f1654c943987cc3e7a
Haga Rakotoharivelo <haga@2befficient.fr>
2023-05-10 18:06:20 +0200

    Add a better rule import (better grouping by filter with new format)

--- src/Fluxzy/Rules/RuleConfigParser.cs
+++ src/Fluxzy/Rules/RuleConfigParser.cs
@@ -200,7 +200,10 @@ namespace Fluxzy.Rules
     {
         public RuleSet(params Rule[] rules)
         {
-            Rules = RuleConfigContainer.CreateFrom(rules).ToList(); 
+            Rules = rules.GroupBy(g => g.Filter.Identifier)
+                         .Select(s => new RuleConfigContainer(s.First().Filter) {
+                             Actions = s.Select(sm => sm.Action).ToList()
+                         }).ToList();
         }
 
         public List<RuleConfigContainer> Rules { get; set; }
