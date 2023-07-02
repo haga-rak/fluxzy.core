ed16502cf1a2e6d097c33679c3c94c2fe2f4b5e1
Haga R <haga.rakotoharivelo@gmail.com>
2023-07-02 13:44:16 +0200

    Update package-version

--- src/Fluxzy.Desktop.Ui/ClientApp/app/auto-update.js
+++ src/Fluxzy.Desktop.Ui/ClientApp/app/auto-update.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.autoUpdateRoutine = void 0;
+exports.getChannelSuffix = exports.getRealPlatform = exports.autoUpdateRoutine = void 0;
 var electron_1 = require("electron");
 var fs = require("fs");
 var path = require("path");
@@ -9,8 +9,10 @@ var autoUpdateRoutine = function (win) {
     if (!packageRun) {
         //return;
     }
+    var platform = (0, exports.getRealPlatform)();
     var server = 'https://releases.fluxzy.io:4433';
-    var url = "".concat(server, "/update/").concat(process.platform, "/").concat(electron_1.app.getVersion());
+    var channelSuffix = (0, exports.getChannelSuffix)(electron_1.app.getVersion());
+    var url = "".concat(server, "/update/").concat(platform, "/").concat(electron_1.app.getVersion());
     electron_1.autoUpdater.setFeedURL({ url: url });
     electron_1.autoUpdater.on('error', function (error) {
         // do nothing
@@ -21,4 +23,25 @@ var autoUpdateRoutine = function (win) {
     }, 10000);
 };
 exports.autoUpdateRoutine = autoUpdateRoutine;
+var getRealPlatform = function () {
+    var platform = process.platform;
+    if (platform === 'win32') {
+        return 'windows_64';
+    }
+    if (platform === 'darwin') {
+        return 'osx_64';
+    }
+    return 'linux_64'; // Auto update is not well supported in linux
+};
+exports.getRealPlatform = getRealPlatform;
+var getChannelSuffix = function (currentAppVersion) {
+    if (currentAppVersion.indexOf('alpha') !== -1) {
+        return '&channel=alpha';
+    }
+    if (currentAppVersion.indexOf('beta') !== -1) {
+        return '&channel=beta';
+    }
+    return '';
+};
+exports.getChannelSuffix = getChannelSuffix;
 //# sourceMappingURL=auto-update.js.map
