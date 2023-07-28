105a353190b04cf956965dd8179ec50919798587
Haga Rakotoharivelo <haga@2befficient.fr>
2023-07-28 16:21:06 +0200

    Validate sample No 001

--- samples/Samples.No001.SimpleCapture/Program.cs
+++ samples/Samples.No001.SimpleCapture/Program.cs
@@ -1,17 +1,55 @@
+using System.Net;
 using Fluxzy;
 
 namespace Samples.No001.SimpleCapture
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		/// <summary>
+		/// In this sample, we will create a simple proxy that will capture all the traffic from
+		/// an HttpClient and save it to an fxzy file or HAR file 
+		/// </summary>
+		static async Task Main()
 		{
-			var fluxzyStartupSetting = new FluxzySetting();
+			var tempDirectory = "capture_dump";
 
+			// Create a default run settings 
+			var fluxzyStartupSetting = FluxzySetting
+			                           .CreateDefault(IPAddress.Loopback, 44344)
+			                           .AddBoundAddress(IPAddress.IPv6Loopback, 44344) // add extra binding address
+			                           .SetOutDirectory(tempDirectory)
+			                           .SetAutoInstallCertificate(true) // Fluxzy will install the certificate to the default machine store
+			                           ;
+			
+			// Create a proxy instance
+			await using (var proxy = new Proxy(fluxzyStartupSetting))
+			{
+				var endpoints = proxy.Run();
 
+				using var httpClient = new HttpClient(new HttpClientHandler()
+				{
+					// We instruct the HttpClient to use the proxy
+					Proxy = new WebProxy($"http://127.0.0.1:{endpoints.First().Port}"),
+					UseProxy = true
+				});
 
+				// Make a request to a remote website
+				using var response = await httpClient.GetAsync("https://www.fluxzy.io/hello");
 
-			Console.WriteLine("Hello, World!");
+				// Fluxzy is in full streaming mode, this means that the actual body content 
+				// is only captured when the client reads it. 
+
+				await (await response.Content.ReadAsStreamAsync()).CopyToAsync(Stream.Null); 
+			}
+
+			// Packing the output files must be after the proxy dispose because some files may 
+			// remain write-locked. 
+
+			// Pack the files into fxzy file. This is the recommended file format as it can holds raw capture datas. 
+			Packager.Export(tempDirectory, "mycapture.fxzy");
+
+			// Pack the files into a HAR file
+			Packager.ExportAsHttpArchive(tempDirectory, "mycapture.har");
 		}
 	}
 }
