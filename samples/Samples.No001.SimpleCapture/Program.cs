af28f03bf13d98466d3f5a1c6581db5be088353c
Haga R <haga.rakotoharivelo@gmail.com>
2023-07-28 15:00:06 +0200

    Update CI configuration (#1)
    * Update CI configuration
    
    * Remove front build from pull-request

---
+++ samples/Samples.No001.SimpleCapture/Program.cs
@@ -0,0 +1,17 @@
+using Fluxzy;
+
+namespace Samples.No001.SimpleCapture
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var fluxzyStartupSetting = new FluxzySetting();
+
+
+
+
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
