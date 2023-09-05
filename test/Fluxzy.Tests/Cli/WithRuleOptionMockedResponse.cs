f2d363b3d0a4857414d5f6dd9b093793f0f2c9cc
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-05 23:14:42 +0200

    Add unit tests for MockedResponse

--- test/Fluxzy.Tests/Cli/WithRuleOptionMockedResponse.cs
+++ test/Fluxzy.Tests/Cli/WithRuleOptionMockedResponse.cs
@@ -1,6 +1,5 @@
 // Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
-using Fluxzy.Tests._Files;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -42,6 +41,34 @@ namespace Fluxzy.Tests.Cli
         }
 
         [Fact]
+        public async Task Validate_MockedResponse_Short()
+        {
+            // Arrange
+            var yamlContent = """
+                rules:
+                - filter: 
+                    typeKind: anyFilter  
+                    operation: endsWith
+                  action : 
+                    typeKind: mockedResponseAction
+                    response:
+                      statusCode: 204
+                """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"https://veryinvalidhost79795-sfsdfdsf.com/cookies");
+
+            // Act 
+            using var response = await Exec(yamlContent, requestMessage);
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.Equal("", responseBody);
+        }
+
+        [Fact]
         public async Task Validate_MockedResponse_With_Header()
         {
             // Arrange
@@ -79,6 +106,41 @@ namespace Fluxzy.Tests.Cli
             Assert.Equal("zaza", zozoValues.First());
 
             Assert.Equal("cocoyes", responseBody);
+        }
+
+        [Fact]
+        public async Task Validate_MockedResponse_With_Type()
+        {
+            // Arrange
+            var yamlContent = """
+                rules:
+                - filter: 
+                    typeKind: anyFilter  
+                    operation: endsWith
+                  action : 
+                    typeKind: mockedResponseAction
+                    response:
+                      statusCode: 200
+                      body:
+                        type: json
+                        text: >
+                          { "coco" : false }
+                """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"https://veryinvalidhost79795-sfsdfdsf.com/cookies");
+
+            // Act 
+            using var response = await Exec(yamlContent, requestMessage);
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var headers = response.Content.Headers;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(headers.TryGetValues("content-type", out var contentTypeValues));
+            Assert.StartsWith("application/json", contentTypeValues.First());
         }
     }
 }
