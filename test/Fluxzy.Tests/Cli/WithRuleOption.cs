fab5dc4e8e9c46881705ecbedb6fa5b1552d5b52
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-05 11:09:16 +0200

    Validate IP ingress

--- test/Fluxzy.Tests/Cli/WithRuleOption.cs
+++ test/Fluxzy.Tests/Cli/WithRuleOption.cs
@@ -15,18 +15,20 @@ using Xunit;
 
 namespace Fluxzy.Tests.Cli
 {
-    public class WithRuleOption
+    public class WithRuleOption : WithRuleOptionBase
     {
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public async Task Run_Cli_With_ClientCertificate(bool forceH11)
+        public async Task Validate_ClientCertificate(bool forceH11)
         {
             // Arrange 
-            var commandLine = "start -l 127.0.0.1/0";
-            var ruleFile = "rules.yml";
+            var requestMessage =
+                new HttpRequestMessage(HttpMethod.Get, $"{TestConstants.GetHost("http2")}/certificate");
 
-            File.WriteAllBytes("cc.pfx", StorageContext.client_cert);
+            requestMessage.Headers.Add("X-Test-Header-256", "That value");
+
+            File.WriteAllBytes($"{nameof(Validate_ClientCertificate)}.pfx", StorageContext.client_cert);
 
             var yamlContent = """
                 rules:
@@ -56,23 +58,8 @@ namespace Fluxzy.Tests.Cli
                       typeKind: ForceHttp11Action
                 """;
 
-            File.WriteAllText(ruleFile, forceH11 ? yamlContentForceHttp11 : yamlContent);
-
-            commandLine += $" -r {ruleFile}";
-
-            var commandLineHost = new FluxzyCommandLineHost(commandLine);
-
-            await using var fluxzyInstance = await commandLineHost.Run();
-            using var proxiedHttpClient = new ProxiedHttpClient(fluxzyInstance.ListenPort);
-
-            var requestMessage =
-                new HttpRequestMessage(HttpMethod.Get, $"{TestConstants.GetHost("http2")}/certificate");
-
-            requestMessage.Headers.Add("X-Test-Header-256", "That value");
-
-            // Act 
-            using var response = await proxiedHttpClient.Client.SendAsync(requestMessage);
-
+            using var response = await Exec(forceH11 ? yamlContentForceHttp11 : yamlContent, requestMessage);
+            
             var thumbPrint = await response.Content.ReadAsStringAsync();
             var expectedThumbPrint = "960b00317d47d0d52d04a3a03b045e96bf3be3a3";
 
@@ -83,12 +70,13 @@ namespace Fluxzy.Tests.Cli
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public async Task Run_Cli_With_ClientCertificate_2(bool forceH11)
+        public async Task Validate_ClientCertificate_2(bool forceH11)
         {
-            // Arrange 
-            var commandLine = "start -l 127.0.0.1/0";
-            var ruleFile = "rules-cert-2.yml";
+            var requestMessage =
+                new HttpRequestMessage(HttpMethod.Get, $"https://certauth.cryptomix.com/json/");
 
+            requestMessage.Headers.Add("X-Test-Header-256", "That value");
+            
             File.WriteAllBytes("cc.pfx", StorageContext.client_cert);
 
             var yamlContent = """
@@ -118,35 +106,17 @@ namespace Fluxzy.Tests.Cli
                     action : 
                       typeKind: ForceHttp11Action
                 """;
-
-            File.WriteAllText(ruleFile, forceH11 ? yamlContentForceHttp11 : yamlContent);
-
-            commandLine += $" -r {ruleFile}";
-
-            var commandLineHost = new FluxzyCommandLineHost(commandLine);
-
-            await using var fluxzyInstance = await commandLineHost.Run();
-            using var proxiedHttpClient = new ProxiedHttpClient(fluxzyInstance.ListenPort);
-
-            var requestMessage =
-                new HttpRequestMessage(HttpMethod.Get, $"https://certauth.cryptomix.com/json/");
-
-            requestMessage.Headers.Add("X-Test-Header-256", "That value");
-
-            // Act 
-            using var response = await proxiedHttpClient.Client.SendAsync(requestMessage);
-
-            var fullResponseString = await response.Content.ReadAsStringAsync();
-
+            
+            using var response = await Exec(forceH11 ? yamlContentForceHttp11 : yamlContent, requestMessage);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
-        public async Task Run_UpdateRequestHeaderAction()
+        public async Task Validate_UpdateRequestHeaderAction()
         {
             // Arrange 
             var commandLine = "start -l 127.0.0.1/0";
-            var ruleFile = $"{nameof(Run_UpdateRequestHeaderAction)}.yml";
+            var ruleFile = $"{nameof(Validate_UpdateRequestHeaderAction)}.yml";
 
             var yamlContent = """
                 rules:
@@ -179,6 +149,37 @@ namespace Fluxzy.Tests.Cli
             await response.Content.ReadAsStringAsync();
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task Validate_IpIngress()
+        {
+            // Arrange
+            var yamlContent = """
+                rules:
+                - filter: 
+                    typeKind: IpIngressFilter  
+                    pattern: 127.0.0.1
+                    operation: exact
+                  action : 
+                    typeKind: UpdateResponseHeaderAction
+                    headerName: fromLocalHost
+                    headerValue: "Relayed by fluxzy"
+                    addIfMissing: true
+                """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"https://www.example.com");
+
+            // Act 
+            using var response = await Exec(yamlContent, requestMessage);
+
+
+            var hasHeader = response.Headers.TryGetValues("fromLocalHost", out var headerValues);
+            var headerValue = headerValues?.FirstOrDefault();
+
+            Assert.True(hasHeader);
+            Assert.Equal("Relayed by fluxzy", headerValue);
 
         }
 
