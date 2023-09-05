83588a4e8fa261a42b27beb6dd5683d3bf3f0d42
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-06 00:43:42 +0200

    Refactory: move namespace and directory

--- src/Fluxzy/Rules/Actions/HighLevelActions/MountWelcomePageAction.cs
+++ src/Fluxzy/Rules/Actions/HighLevelActions/MountWelcomePageAction.cs
@@ -7,7 +7,7 @@ using Fluxzy.Core;
 using Fluxzy.Core.Breakpoints;
 using YamlDotNet.Serialization;
 
-namespace Fluxzy.Rules.Actions
+namespace Fluxzy.Rules.Actions.HighLevelActions
 {
     [ActionMetadata("Reply with fluxzy welcome page")]
     public class MountWelcomePageAction : Action
@@ -24,11 +24,13 @@ namespace Fluxzy.Rules.Actions
             ExchangeContext context, Exchange? exchange, Connection? connection, FilterScope scope,
             BreakPointManager breakPointManager)
         {
-            if (context.PreMadeResponse == null) {
+            if (context.PreMadeResponse == null)
+            {
                 var bodyContent = BodyContent.CreateFromString(FileStore.welcome);
 
                 context.PreMadeResponse = new MockedResponseContent(200,
-                    bodyContent) {
+                    bodyContent)
+                {
                     Headers = {
                         ["Content-Type"] = "text/html"
                     }
