4eaf2589883584d9050b6430c62ebe661682f512
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-05 22:56:02 +0200

    Add bodytype for easying content-type specification

---
+++ src/Fluxzy/Clients/Mock/BodyType.cs
@@ -0,0 +1,40 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+namespace Fluxzy.Clients.Mock
+{
+    public enum BodyType
+    {
+        Text = 1,
+        Json,
+        Xml,
+        Html,
+        Binary,
+        Css,
+        JavaScript = 7,
+        Js = 7,
+        Font = 8, 
+        proto
+
+    }
+    
+    public static class BodyTypeExtensions
+    {
+        public static string? GetContentTypeHeaderValue(this BodyType bodyType)
+        {
+            return bodyType switch
+            {
+                BodyType.Text => "text/plain; charset=utf-8",
+                BodyType.Json => "application/json; charset=utf-8",
+                BodyType.Xml => "application/xml; charset=utf-8",
+                BodyType.Html => "text/html; charset=utf-8",
+                BodyType.Binary => "application/octet-stream",
+                BodyType.Css => "text/css; charset=utf-8",
+                BodyType.JavaScript => "application/javascript; charset=utf-8",
+                BodyType.Font => "application/font-woff2",
+                BodyType.proto => "application/x-protobuf",
+                _ => null
+            };
+
+        }
+    }
+}
