4ae7d8a4c25b82d78bd98c37f704feaf5d16f739
Haga Rakotoharivelo <haga@2befficient.fr>
2023-09-09 23:33:06 +0200

    Add equality comparer for AuthorityInfo

--- src/Fluxzy/Archiving/AuthorityInfo.cs
+++ src/Fluxzy/Archiving/AuthorityInfo.cs
@@ -1,5 +1,6 @@
-﻿// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
+using System;
 using System.Text.Json.Serialization;
 using Fluxzy.Core;
 
@@ -27,5 +28,36 @@ namespace Fluxzy
         public int Port { get; set; }
 
         public bool Secure { get; set; }
+
+        protected bool Equals(AuthorityInfo other)
+        {
+            return HostName == other.HostName && Port == other.Port && Secure == other.Secure;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((AuthorityInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(HostName, Port, Secure);
+        }
+
     }
 }
