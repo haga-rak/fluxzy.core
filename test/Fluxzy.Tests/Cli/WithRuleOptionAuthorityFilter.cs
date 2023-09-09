9ee8bf428843ad4ad49c5166cdcf7bdffb607c02
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-10 01:59:46 +0200

    Fix AuthorityFilter bad evaluation when used with ExchangeInfo (#27)
    * Fix AuthorityFilter bad evaluation when used with ExchangeInfo
    
    * Add AuthorityFilter unit test

---
+++ test/Fluxzy.Tests/Cli/WithRuleOptionAuthorityFilter.cs
@@ -0,0 +1,68 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fluxzy.Tests._Fixtures;
+using Xunit;
+
+namespace Fluxzy.Tests.Cli
+{
+    public class WithRuleOptionAuthorityFilter : WithRuleOptionBase
+    {
+        [Fact]
+        public async Task Validate_AuthorityFilter()
+        {
+            // Arrange
+            var yamlContent = $"""
+                               rules:
+                               - filter:
+                                   typeKind: AuthorityFilter
+                                   pattern: www.example.com
+                                   port: 443
+                                   operation: exact
+                                 action :
+                                   typeKind: addResponseHeaderAction
+                                   headerName: Passed
+                                   headerValue: true
+                               """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"https://www.example.com/");
+
+            // Act
+            using var response = await Exec(yamlContent, requestMessage, allowAutoRedirect: false);
+            await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.True(response.Headers.TryGetValues("Passed", out  _));
+        }
+
+        [Fact]
+        public async Task Validate_AuthorityFilter_Not_Pass()
+        {
+            // Arrange
+            var yamlContent = $"""
+                               rules:
+                               - filter:
+                                   typeKind: AuthorityFilter
+                                   pattern: www.exdmple.com
+                                   port: 443
+                                   operation: exact
+                                 action :
+                                   typeKind: addResponseHeaderAction
+                                   headerName: Passed
+                                   headerValue: true
+                               """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"https://www.example.com/");
+
+            // Act
+            using var response = await Exec(yamlContent, requestMessage, allowAutoRedirect: false);
+            await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.False(response.Headers.TryGetValues("Passed", out  _));
+        }
+    }
+}
