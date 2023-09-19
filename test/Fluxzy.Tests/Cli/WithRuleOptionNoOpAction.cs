55f4b77ee50142a341deb1947ea7fb20c723f570
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-20 01:33:33 +0200

    Add new action: `NoOpAction` (#33)
    * Fix invalid method name
    
    * Add NoOp Action
    
    * Add unit tests for NoOp
    
    * Update docs for NoOpAction

---
+++ test/Fluxzy.Tests/Cli/WithRuleOptionNoOpAction.cs
@@ -0,0 +1,51 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fluxzy.Tests.Cli
+{
+    public class WithRuleOptionNoOpAction : WithRuleOptionBase
+    {
+        [Fact]
+        public async Task Validate_Simple()
+        {
+            // Arrange
+            var yamlContent = $"""
+                               rules:
+                               - filter:
+                                   typeKind: AnyFilter
+                                 action :
+                                   typeKind: noOpAction
+                               """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"https://www.example.com/");
+
+            // Act
+            using var response = await Exec(yamlContent, requestMessage, allowAutoRedirect: false);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Validate_Simple_2()
+        {
+            // Arrange
+            var yamlContent = $"""
+                               rules:
+                               - filter:
+                                   typeKind: AnyFilter
+                                 actions:
+                                   - typeKind: noOpAction
+                               """;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                $"https://www.example.com/");
+
+            // Act
+            using var response = await Exec(yamlContent, requestMessage, allowAutoRedirect: false);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
