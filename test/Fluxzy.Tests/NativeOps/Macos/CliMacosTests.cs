e6e9e779992375bfa7748d1ee3587f9d8bd20d29
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-23 23:48:28 +0200

    Remove obsolete unit tests

--- test/Fluxzy.Tests/NativeOps/Macos/CliMacosTests.cs
+++ test/Fluxzy.Tests/NativeOps/Macos/CliMacosTests.cs
@@ -8,60 +8,8 @@ namespace Fluxzy.Tests.NativeOps.Macos
 {
     public class CliMacosTests
     {
-        private static readonly string OriginalCommandLine =
-            "An asterisk (*) denotes that a network service is disabled.\r\n(1) Ethernet\r\n(Hardware Port: Ethernet, Device: en0)\r\n\r\n(2) FireWire\r\n(Hardware Port: FireWire, Device: fw0)\r\n\r\n(3) Wi-Fi\r\n(Hardware Port: Wi-Fi, Device: en1)\r\n\r\n(4) iPhone\r\n(Hardware Port: USB iPhone, Device: en4)\r\n\r\n(5) Bluetooth PAN\r\n(Hardware Port: Bluetooth PAN, Device: en3)\r\n\r\n(6) Thunderbolt Bridge\r\n(Hardware Port: Thunderbolt Bridge, Device: bridge0)\r\n";
-
         private readonly string _basicCommandLIneResult
             = "\r\nEnabled: yes\r\nServer: 127.0.0.1\r\nPort: 44344\r\nAuthenticated Proxy Enabled: 0";
-
-        [Fact]
-        public void TestParsingListNetworkServiceOrder()
-        {
-            var cmdLineResult = MacOsHelper.ParseInterfaces(OriginalCommandLine).ToList();
-
-            Assert.Equal(6, cmdLineResult.Count);
-            Assert.Contains(cmdLineResult, i => i.Name == "Ethernet");
-            Assert.Contains(cmdLineResult, i => i.Name == "FireWire");
-            Assert.Contains(cmdLineResult, i => i.Name == "Wi-Fi");
-            Assert.Contains(cmdLineResult, i => i.Name == "iPhone");
-            Assert.Contains(cmdLineResult, i => i.Name == "Bluetooth PAN");
-            Assert.Contains(cmdLineResult, i => i.Name == "Thunderbolt Bridge");
-
-        }
-
-        [Fact]
-        public void TestParsingListNetworkServiceOrderMacOsNewLine()
-        {
-            var commandLine = OriginalCommandLine.Replace("\r\n", "\r");
-
-            var cmdLineResult = MacOsHelper.ParseInterfaces(commandLine).ToList();
-
-            Assert.Equal(6, cmdLineResult.Count);
-            Assert.Contains(cmdLineResult, i => i.Name == "Ethernet");
-            Assert.Contains(cmdLineResult, i => i.Name == "FireWire");
-            Assert.Contains(cmdLineResult, i => i.Name == "Wi-Fi");
-            Assert.Contains(cmdLineResult, i => i.Name == "iPhone");
-            Assert.Contains(cmdLineResult, i => i.Name == "Bluetooth PAN");
-            Assert.Contains(cmdLineResult, i => i.Name == "Thunderbolt Bridge");
-
-        }
-
-        [Fact]
-        public void TestParsingListNetworkServiceOrderUnixNewLine()
-        {
-            var commandLine = OriginalCommandLine.Replace("\r\n", "\n");
-
-            var cmdLineResult = MacOsHelper.ParseInterfaces(commandLine).ToList();
-
-            Assert.Equal(6, cmdLineResult.Count);
-            Assert.Contains(cmdLineResult, i => i.Name == "Ethernet");
-            Assert.Contains(cmdLineResult, i => i.Name == "FireWire");
-            Assert.Contains(cmdLineResult, i => i.Name == "Wi-Fi");
-            Assert.Contains(cmdLineResult, i => i.Name == "iPhone");
-            Assert.Contains(cmdLineResult, i => i.Name == "Bluetooth PAN");
-            Assert.Contains(cmdLineResult, i => i.Name == "Thunderbolt Bridge");
-
-        }
 
         [Fact]
         public void TestParseProxySettings()
