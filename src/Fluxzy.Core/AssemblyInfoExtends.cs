2c70c909ba9a16838c23789310ba685ce5b2d58a
Haga R <haga.rakotoharivelo@gmail.com>
2023-09-29 14:21:43 +0200

    Refactor project names and directories for consistency (#40)
    * Swap folders for CLI and Core
    
    * Re-reference new project names
    
    * Add various renaming
    
    * Update CI flows
    
    * Add test repetition for websocket
    
    * Remove more reference to fxzy binary

--- src/Fluxzy.Core/AssemblyInfoExtends.cs
+++ src/Fluxzy.Core/AssemblyInfoExtends.cs
@@ -3,13 +3,12 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Fluxzy.Cli")]
+[assembly: InternalsVisibleTo("fluxzy")]
 [assembly: InternalsVisibleTo("Fluxzy.Desktop.Services")]
 [assembly: InternalsVisibleTo("Fluxzy.Desktop.Ui")]
 [assembly: InternalsVisibleTo("Fluxzy.Encoding.Tests")]
 [assembly: InternalsVisibleTo("Fluxzy.Bulk.BcCli")]
 [assembly: InternalsVisibleTo("Fluxzy.Tests")]
-[assembly: InternalsVisibleTo("fxzy")]
 
 // ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
--- src/Fluxzy.Core/Certificates/FluxzySecurity.cs
+++ src/Fluxzy.Core/Certificates/FluxzySecurity.cs
@@ -1,6 +1,7 @@
 // Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
 using System.Security.Cryptography.X509Certificates;
+using Fluxzy.Core;
 
 namespace Fluxzy.Certificates
 {
