a37a79daa866cfed50202e698f9a8ee10f68b022
Haga R <haga.rakotoharivelo@gmail.com>
2023-10-08 02:13:40 +0200

    Add certificate building logic

---
+++ src/Fluxzy.Core/Certificates/CertificateBuilder.cs
@@ -0,0 +1,90 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Fluxzy.Certificates
+{
+    public class CertificateBuilder
+    {
+        private readonly CertificateBuilderOptions _options;
+
+        public CertificateBuilder(CertificateBuilderOptions options)
+        {
+            options.Validate();
+            _options = options; 
+        }
+
+        public byte[] CreateSelfSigned()
+        {
+            using var privateKey = RSA.Create(_options.KeySize);
+            
+            var certificateRequest = new CertificateRequest(
+                _options.Format(),
+                privateKey,
+                HashAlgorithmName.SHA256,
+                RSASignaturePadding.Pkcs1);
+
+            certificateRequest.CertificateExtensions.Add(
+                new X509KeyUsageExtension(
+                    X509KeyUsageFlags.KeyCertSign
+                    | X509KeyUsageFlags.CrlSign
+                    | X509KeyUsageFlags.DigitalSignature,
+                    false));
+
+            certificateRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 10, false));
+
+            certificateRequest.CertificateExtensions.Add(
+                new X509EnhancedKeyUsageExtension(
+                    new OidCollection {
+                        new("1.3.6.1.5.5.7.3.1"),
+                        new("1.3.6.1.5.5.7.3.2")
+                    },
+                    false));
+
+            byte[] data = new byte[20];
+
+            new Random().NextBytes(data);
+
+            var subjectExtension = new X509SubjectKeyIdentifierExtension(certificateRequest.PublicKey, false);
+
+            certificateRequest.CertificateExtensions.Add(subjectExtension);
+            certificateRequest.CertificateExtensions.Add(
+                new X509AuthorityKeyIdentifierExtension2(subjectExtension.RawData, false));
+
+            using var cert =
+                certificateRequest.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddDays(
+                    _options.DaysBeforeExpiration));
+
+            if (_options.P12Password != null)
+                cert.Export(X509ContentType.Pkcs12, _options.P12Password);
+
+            return cert.Export(X509ContentType.Pkcs12);
+        }
+    }
+
+    internal class X509AuthorityKeyIdentifierExtension2 : X509Extension
+    {
+        private static Oid AuthorityKeyIdentifierOid => new Oid("2.5.29.35");
+
+        public X509AuthorityKeyIdentifierExtension2(byte[] subjectIdentifierRawData, bool critical)
+            : base(AuthorityKeyIdentifierOid, EncodeExtension(subjectIdentifierRawData), critical)
+        {
+        }
+
+        private static byte[] EncodeExtension(byte[] subjectIdentifierRawData)
+        {
+            var rawData = subjectIdentifierRawData;
+            var segment = new ArraySegment<byte>(rawData, 2, rawData.Length - 2);
+            var authorityKeyIdentifier = new byte[segment.Count + 4];
+            // KeyID of the AuthorityKeyIdentifier
+            authorityKeyIdentifier[0] = 0x30;
+            authorityKeyIdentifier[1] = 0x16;
+            authorityKeyIdentifier[2] = 0x80;
+            authorityKeyIdentifier[3] = 0x14;
+            segment.CopyTo(authorityKeyIdentifier, 4);
+            return authorityKeyIdentifier;
+        }
+    }
+}
---
+++ src/Fluxzy.Core/Certificates/CertificateBuilderOptions.cs
@@ -0,0 +1,88 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System;
+using System.Text;
+
+namespace Fluxzy.Certificates
+{
+    public class CertificateBuilderOptions
+    {
+        public CertificateBuilderOptions(string commonName)
+        {
+            CommonName = commonName;
+        }
+
+        public string CommonName { get; }
+
+        public string? Locality { get; set; }
+
+        public string? State { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? Organization { get; set; }
+
+        public string? OrganizationUnit { get; set; }
+
+        public int DaysBeforeExpiration { get; set; } = 365 * 10;
+
+        public string? P12Password { get; set; }
+
+        public int KeySize { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(CommonName))
+                throw new ArgumentException("CommonName is required");
+
+            // KeySize must be a multiple of 1024 and less or equal to 16384
+            // Control that any certificate subject attribute is valid (no comma)  
+
+            if (KeySize % 1024 != 0 || KeySize > 16384 || KeySize < 1024)
+                throw new ArgumentException("KeySize must be a multiple of 1024 and less or equal to 16384");
+
+            if (CommonName.Contains(","))
+                throw new ArgumentException("CommonName cannot contain comma");
+
+            if (Locality != null && Locality.Contains(","))
+                throw new ArgumentException("Locality cannot contain comma");
+
+            if (Country != null && Country.Contains(","))
+                throw new ArgumentException("Country cannot contain comma");
+
+            if (Organization != null && Organization.Contains(","))
+                throw new ArgumentException("Organization cannot contain comma");
+
+            if (OrganizationUnit != null && OrganizationUnit.Contains(","))
+                throw new ArgumentException("OrganizationUnit cannot contain comma");
+
+            if (DaysBeforeExpiration <= 0)
+                throw new ArgumentException("DaysBeforeExpiration cannot be negative or zero");
+        }
+
+        public string Format()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append($"CN={CommonName}");
+
+            if (Locality != null)
+                builder.Append($", L={Locality}");
+
+            if (Country != null)
+                builder.Append($", C={Country}");
+
+            if (Organization != null)
+                builder.Append($", O={Organization}");
+
+            if (State != null)
+                builder.Append($", O={State}");
+
+            if (OrganizationUnit != null)
+                builder.Append($", OU={OrganizationUnit}");
+
+            return builder.ToString();
+        }
+
+    }
+}
