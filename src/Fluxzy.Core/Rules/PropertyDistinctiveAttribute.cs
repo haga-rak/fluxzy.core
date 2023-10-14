6a1eab4180a06ade95f8e6ecd3df48814ce19771
Haga R <haga.rakotoharivelo@gmail.com>
2023-10-15 01:29:25 +0200

    Fix property table breaking, handle default values (#64)

---
+++ src/Fluxzy.Core/Rules/PropertyDistinctiveAttribute.cs
@@ -0,0 +1,20 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System;
+
+namespace Fluxzy.Rules
+{
+    public class PropertyDistinctiveAttribute : Attribute
+    {
+        public bool Expand { get; set; }
+
+        public bool IgnoreInDoc { get; set; } = false;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string? DefaultValue { get; set; }
+
+        public string ? FriendlyType { get; set; }
+
+    }
+}
