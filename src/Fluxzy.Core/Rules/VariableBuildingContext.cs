58b3d44ad22d6ca69984ec105be55282c0567b0d
Haga R <haga.rakotoharivelo@gmail.com>
2023-11-24 19:06:57 +0100

    Update API docs

--- src/Fluxzy.Core/Rules/VariableBuildingContext.cs
+++ src/Fluxzy.Core/Rules/VariableBuildingContext.cs
@@ -6,6 +6,9 @@ using Fluxzy.Core;
 
 namespace Fluxzy.Rules
 {
+    /// <summary>
+    ///    Holds variables datas during an exchange processing
+    /// </summary>
     public class VariableBuildingContext
     {
         public VariableBuildingContext(
@@ -26,6 +29,7 @@ namespace Fluxzy.Rules
 
             LazyVariableEvaluations["exchange.id"] = () => exchange?.Id.ToString() ?? string.Empty;
             LazyVariableEvaluations["exchange.url"] = () => exchange?.FullUrl ?? string.Empty;
+            LazyVariableEvaluations["exchange.instant"] = () => exchange?.Metrics.ReceivedFromProxy.ToString("O") ?? string.Empty;
 
             LazyVariableEvaluations["exchange.method"] =
                 () => exchange?.Request.Header.Method.ToString() ?? string.Empty;
@@ -36,12 +40,25 @@ namespace Fluxzy.Rules
                 exchange?.StatusCode > 0 ? exchange.StatusCode.ToString() : string.Empty;
         }
 
+        /// <summary>
+        ///   Current exchange context
+        /// </summary>
         public ExchangeContext ExchangeContext { get; }
 
+        /// <summary>
+        ///   The processed exchange if any
+        /// </summary>
         public Exchange? Exchange { get; }
 
+        /// <summary>
+        ///   The used connection
+        /// </summary>
         public Connection? Connection { get; }
 
+
+        /// <summary>
+        ///   Variable evaluations that are lazy evaluated
+        /// </summary>
         public IDictionary<string, Func<string>> LazyVariableEvaluations { get; }
     }
 }
