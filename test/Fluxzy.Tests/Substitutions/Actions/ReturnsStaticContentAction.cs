27de095ab7ae7a9dcbcce93fd907fb3ee4cf888f
Haga R <haga.rakotoharivelo@gmail.com>
2023-11-24 15:41:12 +0100

    Add unit tests

---
+++ test/Fluxzy.Tests/Substitutions/Actions/ReturnsStaticContentAction.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Fluxzy.Core;
+using Fluxzy.Core.Breakpoints;
+using Fluxzy.Rules;
+
+namespace Fluxzy.Tests.Substitutions.Actions
+{
+    /// <summary>
+    /// A simple action that returns the content length of the original response instead of the response
+    /// </summary>
+    internal class ReturnsStaticContentAction : Action
+    {
+        private readonly string _content;
+
+        public ReturnsStaticContentAction(string content)
+        {
+            _content = content;
+        }
+
+        public override FilterScope ActionScope { get; } = FilterScope.RequestHeaderReceivedFromClient;
+
+        public override string DefaultDescription { get; } = "Test action";
+
+        public override ValueTask InternalAlter(
+            ExchangeContext context, Exchange? exchange, Connection? connection, FilterScope scope,
+            BreakPointManager breakPointManager)
+        {
+            context.ResponseBodySubstitution = new ReturnsContentLengthSubstitution(_content);
+            return default;
+        }
+    }
+
+    internal class ReturnsContentLengthSubstitution : IStreamSubstitution
+    {
+        private readonly string _content;
+
+        public ReturnsContentLengthSubstitution(string content)
+        {
+            _content = content;
+        }
+
+        public Stream Substitute(Stream stream)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(_content));
+        }
+    }
+}
