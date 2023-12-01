432b8e22b424818f0a47c6a14777bca5637d4b4d
Haga <haga.rakotoharivelo@gmail.com>
2023-12-01 18:00:56 +0100

    Remove nullable warning for serialization only object

---
+++ src/Fluxzy.Tools.DocGen/SearchableItem.cs
@@ -0,0 +1,17 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+namespace Fluxzy.Tools.DocGen
+{
+    public class SearchableItem
+    {
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string FullTypeName { get; set; } = null!;
+
+        public string Category { get; set; } = null!;
+
+        public string Scope { get; set; } = null!;
+    }
+}
