f599358d6b3510f06e59c5fb9c94d54f9cb6c27a
Haga R <haga.rakotoharivelo@gmail.com>
2023-12-06 16:07:32 +0100

    Handle early close notify on SslStream for H11 connections (#107)
    * Add helper to retrieve env var and parse in a single go
    
    * Detect early close notify for H11

---
+++ test/Fluxzy.Tests/Cases/EarlyCloseNotify.cs
@@ -0,0 +1,43 @@
+using Fluxzy.Tests._Fixtures;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fluxzy.Tests.Cases
+{
+    public class EarlyCloseNotify
+    {
+        [Theory]
+        [InlineData("BouncyCastle")]
+        [InlineData("OSDefault")]
+        public async Task Run_Until_Close_Notify(string sslEngine)
+        {
+            var count = 10;
+            var url = "https://espace-client.mma.fr/connexion/main-es2015.b75177084686b8f6f756.js"; 
+
+            await using var proxy = new AddHocConfigurableProxy(1, 10);
+
+            proxy.StartupSetting.UseBouncyCastle = sslEngine == "BouncyCastle";
+
+            var endPoint = proxy.Run().First();
+
+            using var clientHandler = new HttpClientHandler();
+
+            clientHandler.Proxy = new WebProxy($"http://{endPoint}");
+
+            using var httpClient = new HttpClient(clientHandler);
+
+            for (int i = 0; i < count; i++) {
+                var requestMessage = new HttpRequestMessage(HttpMethod.Get,
+                                       url);
+
+                using var response = await httpClient.SendAsync(requestMessage);
+                _ = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+    }
+}
