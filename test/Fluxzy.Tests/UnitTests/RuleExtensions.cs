6c6d98063fca9392504d7eb0d9818df00bfa22a2
fluxzy-ci <admin@fluxzy.io>
2023-12-08 13:37:59 +0100

    Remove unused import

--- test/Fluxzy.Tests/UnitTests/RuleExtensions.cs
+++ test/Fluxzy.Tests/UnitTests/RuleExtensions.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Fluxzy.Rules.Actions;
 using Fluxzy.Rules.Filters;
 using Fluxzy.Rules.Filters.RequestFilters;
