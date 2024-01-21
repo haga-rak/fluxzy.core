955ef8210cf5b107f6330cea5ec900a3daf2f0ff
Haga R <haga.rakotoharivelo@gmail.com>
2024-01-21 22:35:48 +0100

    Add more control on ExchangeInfo content

--- test/Fluxzy.Tests/UnitTests/Events/EventWriterTests.cs
+++ test/Fluxzy.Tests/UnitTests/Events/EventWriterTests.cs
@@ -14,33 +14,77 @@ namespace Fluxzy.Tests.UnitTests.Events
     /// </summary>
     public class EventWriterTests
     {
-        [Fact]
-        public async void ExchangeUpdated_Complete()
+        [Theory]
+        [InlineData(ArchiveUpdateType.BeforeRequestHeader)]
+        [InlineData(ArchiveUpdateType.AfterResponseHeader)]
+        [InlineData(ArchiveUpdateType.AfterResponse)]
+        public async Task ExchangeUpdated_ArchiveUpdateStatus(ArchiveUpdateType updateType)
         {
             var maxTimeoutSeconds = TimeoutConstants.Short; 
             var fluxzySetting = FluxzySetting.CreateDefault(IPAddress.Loopback, 0);
 
             await using (var proxy = new Proxy(fluxzySetting)) {
-                var endPoint = proxy.Run().First();
                 var taskCompletionSource = new TaskCompletionSource(); 
 
                 proxy.Writer.ExchangeUpdated += (_, args) => {
                     
-                    if (args.UpdateType == ArchiveUpdateType.Complete)
+                    if (args.UpdateType == updateType)
                         taskCompletionSource.SetResult();
                 };
 
+                var endPoint = proxy.Run().First();
+
                 using var client = HttpClientHelper.Create(endPoint);
 
-                await client.GetAsync("https://example.com"); 
+                await client.GetAsync("https://example.com");
+
+                var delayTask = Task.Delay(maxTimeoutSeconds * 1000);
+
+                await Task.WhenAny(taskCompletionSource.Task, delayTask);
                 
-                await Task.WhenAny(taskCompletionSource.Task,
-                    Task.Delay(maxTimeoutSeconds * 1000));
+                Assert.False(delayTask.IsCompletedSuccessfully);
+            }
+        }
+
+        [Fact]
+        public async Task ExchangeUpdated_Control_Content()
+        {
+            var maxTimeoutSeconds = TimeoutConstants.Short; 
+            var fluxzySetting = FluxzySetting.CreateDefault(IPAddress.Loopback, 0);
+            var url = "https://sandbox.smartizy.com/global-health-check"; 
+
+            await using (var proxy = new Proxy(fluxzySetting)) {
+                var taskCompletionSource = new TaskCompletionSource();
+
+                ExchangeInfo? exchangeInfo = null; 
+
+                proxy.Writer.ExchangeUpdated += (_, args) => {
+                    
+                    if (args.UpdateType == ArchiveUpdateType.AfterResponse)
+                        taskCompletionSource.SetResult();
+
+                    exchangeInfo = args.ExchangeInfo; 
+                };
+
+                var endPoint = proxy.Run().First();
+
+                using var client = HttpClientHelper.Create(endPoint);
+
+                await client.GetAsync(url);
+
+                var delayTask = Task.Delay(maxTimeoutSeconds * 1000);
+
+                await Task.WhenAny(taskCompletionSource.Task, delayTask);
                 
-                Assert.True(taskCompletionSource.Task.IsCompleted);
+                Assert.False(delayTask.IsCompletedSuccessfully);
+                Assert.NotNull(exchangeInfo); 
+                Assert.Equal(200, exchangeInfo.StatusCode); 
+                Assert.Equal(url, exchangeInfo.FullUrl); 
             }
         }
     }
+
+
 
     internal static class HttpClientHelper
     {
