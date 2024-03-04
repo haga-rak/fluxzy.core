2c645a2bf39965a25d2d5dbf05748bb671dc1ed8
Haga R. <haga.rakotoharivelo@gmail.com>
2024-03-04 23:17:56 +0100

    Add more coverage

--- test/Fluxzy.Tests/UnitTests/CookieFlows/CookieFlowAnalyzerTests.cs
+++ test/Fluxzy.Tests/UnitTests/CookieFlows/CookieFlowAnalyzerTests.cs
@@ -6,8 +6,10 @@ namespace Fluxzy.Tests.UnitTests.CookieFlows
 {
     public class CookieFlowAnalyzerTests
     {
-        [Fact]
-        public void Test()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("/")]
+        public void Add_Remove_From_Server(string? path)
         {
             var analyzer = new CookieFlowAnalyzer();
 
@@ -15,8 +17,29 @@ namespace Fluxzy.Tests.UnitTests.CookieFlows
             var fxzyArchiveReader = new FluxzyArchiveReader(url);
             var exchanges = fxzyArchiveReader.ReadAllExchanges().ToList();
 
-            var result = analyzer.Execute("abc", "httpbin.org", null, exchanges);
+            var result = analyzer.Execute("freeform", "httpbin.org", path, exchanges);
 
+            Assert.Equal(6, result.Events.Count);
+            Assert.Equal(CookieUpdateType.AddedFromServer, result.Events[0].UpdateType);
+            Assert.Equal(CookieUpdateType.RemovedByServer, result.Events[5].UpdateType);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("/")]
+        public void Add_Remove_From_Client(string? path)
+        {
+            var analyzer = new CookieFlowAnalyzer();
+
+            var url = "_Files/Archives/sample-bin.fxzy";
+            var fxzyArchiveReader = new FluxzyArchiveReader(url);
+            var exchanges = fxzyArchiveReader.ReadAllExchanges().ToList();
+
+            var result = analyzer.Execute("abc", "httpbin.org", path, exchanges);
+
+            Assert.Equal(5, result.Events.Count);
+            Assert.Equal(CookieUpdateType.AddedFromServer, result.Events[0].UpdateType);
+            Assert.Equal(CookieUpdateType.RemovedByClient, result.Events[4].UpdateType);
         }
     }
 }
