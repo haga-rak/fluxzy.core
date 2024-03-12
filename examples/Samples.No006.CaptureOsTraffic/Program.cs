f4a9901ea828b04f78f76004e4935849c6030507
Haga R. <haga.rakotoharivelo@gmail.com>
2024-03-12 14:00:26 +0100

    Add registering as system proxy

---
+++ examples/Samples.No006.CaptureOsTraffic/Program.cs
@@ -0,0 +1,31 @@
+using Fluxzy;
+using Fluxzy.Core;
+
+namespace Samples.No006.CaptureOsTraffic
+{
+    internal class Program
+    {
+        /// <summary>
+        ///  Registering as system proxy
+        /// </summary>
+        static async Task Main()
+        {
+            // Create a default run settings 
+            var fluxzyStartupSetting = FluxzySetting.CreateLocalRandomPort();
+
+            // Create a proxy instance
+            await using var proxy = new Proxy(fluxzyStartupSetting);
+
+            var endpoints = proxy.Run();
+
+            await using var proxyRegistration = await SystemProxyRegistrationHelper.Create(endpoints.First());
+
+            // Fluxzy is now registered as the system proxy, the proxy will revert
+            // back to the original settings when proxyRegistration is disposed.
+
+            Console.WriteLine("Press any key to halt proxy and unregistered"); 
+
+            Console.ReadKey();
+        }
+    }
+}
