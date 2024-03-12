10ae5ce6ad6733f4af952393b56625adab76b651
Haga R. <haga.rakotoharivelo@gmail.com>
2024-03-12 14:15:44 +0100

    Add `InjectCodeSnippet` example

---
+++ examples/Samples.No008.InjectCodeSnippet/Program.cs
@@ -0,0 +1,36 @@
+using Fluxzy;
+using Fluxzy.Rules.Actions.HighLevelActions;
+using Fluxzy.Rules.Filters.RequestFilters;
+
+namespace Samples.No008.InjectCodeSnippet
+{
+    internal class Program
+    {
+        /// <summary>
+        ///  Inject a code snippet into traversing html content
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {
+            var fluxzySetting = FluxzySetting.CreateDefault();
+
+            fluxzySetting.ConfigureRule()
+                         // When the content type is text/html
+                         .When(new RequestHeaderFilter("text/html", "content-type"))
+
+                         // Inject the following html tag into the head tag
+                         .Do(new InjectHtmlTagAction()
+                         {
+                             HtmlContent = "<style>body { background-color: red !important; }</style>",
+                             Tag = "head",
+                         });
+
+            await using var proxy = new Proxy(fluxzySetting);
+
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+    }
+}
