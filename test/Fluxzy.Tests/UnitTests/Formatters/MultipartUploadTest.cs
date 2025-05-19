d695eed2d34df4c87ddd7d2c63c40afa577f3286
Haga R. <haga.rakotoharivelo@gmail.com>
2025-05-19 19:49:50 +0200

    Fix boundary quotes ignored in multipart request payload #418 (#420)

---
+++ test/Fluxzy.Tests/UnitTests/Formatters/MultipartUploadTest.cs
@@ -0,0 +1,51 @@
+// Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
+
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Fluxzy.Formatters;
+using Fluxzy.Tests._Fixtures;
+using Xunit;
+
+namespace Fluxzy.Tests.UnitTests.Formatters
+{
+    public class MultipartUploadTest
+    {
+        [Fact]
+        public async Task Validate()
+        {
+            var fakeBuffer = "helloWorld".ToBytes(Encoding.UTF8);
+
+            using var content = new MultipartFormDataContent
+            {
+                { new ByteArrayContent(fakeBuffer), "file", "image.png" }
+            };
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://sandbox.smartizy.com/global-health-check")
+            {
+                Content = content
+            };
+
+            var outFileName = "archive.fxzy";
+
+            await QuickArchiveBuilder.MakeQuickArchive(requestMessage, outFileName);
+
+            var archiveReaderProvider = new FromFileArchiveFileProvider(outFileName);
+            var archiveReader = (await archiveReaderProvider.Get())!;
+
+            var producerFactory = new ProducerFactory(archiveReaderProvider, FormatSettings.Default);
+            var firstExchange = archiveReader.ReadAllExchanges().First();
+
+            using var context = await producerFactory.GetProducerContext(firstExchange.Id);
+
+            Assert.NotNull(context);
+            Assert.NotNull(context.GetContextInfo());
+            Assert.NotNull(context.GetContextInfo().ResponseBodyLength);
+            Assert.NotNull(context.GetContextInfo().ResponseBodyText);
+            Assert.True(context.GetContextInfo().IsTextContent);
+
+            var result =  producerFactory.GetRequestFormattedResults(firstExchange.Id, context).ToList();
+        }
+    }
+}
