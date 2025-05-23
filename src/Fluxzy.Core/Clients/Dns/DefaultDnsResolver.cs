246097b57ae523879ae40c69b963c29031296eda
haga-rak <haga.rakotoharivelo@gmail.com>
2025-05-23 11:43:22 +0200

    Add more safeguard for remove window

--- src/Fluxzy.Core/Clients/Dns/DefaultDnsResolver.cs
+++ src/Fluxzy.Core/Clients/Dns/DefaultDnsResolver.cs
@@ -18,7 +18,7 @@ namespace Fluxzy.Clients.Dns
 
         public async Task<IReadOnlyCollection<IPAddress>> SolveDnsAll(string hostName)
         {
-            using var @lock = await Synchronizer<string>.Shared.LockAsync(hostName).ConfigureAwait(false);
+            using var _ = await Synchronizer<string>.Shared.LockAsync(hostName).ConfigureAwait(false);
 
             if (_cache.TryGetValue(hostName, out var cached))
                 return cached;
