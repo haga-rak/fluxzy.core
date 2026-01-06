56dd3041401971f53b488918c050cfa730ee68ff
haga-rak <haga.rakotoharivelo@gmail.com>
2026-01-06 16:49:32 +0100

    Add Android emulator host (10.0.2.2) support for IsSelfFilter
      When Fluxzy is used with Android emulators, the emulator accesses the
      host machine via 10.0.2.2. This change adds a new setting
      `IncludeAndroidEmulatorHost` (default: true) that makes IsSelfFilter
      recognize 10.0.2.2 as a local address.
    
      - Add IncludeAndroidEmulatorHost property to FluxzySetting
      - Add SetIncludeAndroidEmulatorHost fluent setter
      - Add --no-android-emulator CLI option to disable the feature
      - Update IsSelfFilter to check for 10.0.2.2 when enabled
      - Add unit tests for the new functionality

--- test/Fluxzy.Tests/Cases/SelfCallTests.cs
+++ test/Fluxzy.Tests/Cases/SelfCallTests.cs
@@ -1,5 +1,6 @@
 // Copyright 2021 - Haga Rakotoharivelo - https://github.com/haga-rak
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -8,6 +9,9 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Fluxzy.Certificates;
+using Fluxzy.Clients;
+using Fluxzy.Core;
+using Fluxzy.Rules;
 using Fluxzy.Rules.Actions.HighLevelActions;
 using Fluxzy.Rules.Filters;
 using Fluxzy.Rules.Filters.RequestFilters;
@@ -17,6 +21,90 @@ namespace Fluxzy.Tests.Cases
 {
     public class SelfCallTests
     {
+        [Fact]
+        public void IncludeAndroidEmulatorHost_DefaultValue_IsTrue()
+        {
+            // The default value for IncludeAndroidEmulatorHost should be true
+            var setting = FluxzySetting.CreateLocalRandomPort();
+            Assert.True(setting.IncludeAndroidEmulatorHost);
+        }
+
+        [Fact]
+        public void IncludeAndroidEmulatorHost_CanBeDisabled()
+        {
+            var setting = FluxzySetting.CreateLocalRandomPort();
+            setting.SetIncludeAndroidEmulatorHost(false);
+            Assert.False(setting.IncludeAndroidEmulatorHost);
+        }
+
+        [Fact]
+        public void AndroidEmulatorHostAddress_IsCorrect()
+        {
+            // Verify the Android emulator host address is 10.0.2.2
+            Assert.Equal(IPAddress.Parse("10.0.2.2"), IsSelfFilter.AndroidEmulatorHostAddress);
+        }
+
+        [Fact]
+        public void IsSelfFilter_WithAndroidEmulatorEnabled_MatchesEmulatorAddress()
+        {
+            // Arrange
+            var setting = FluxzySetting.CreateLocalRandomPort();
+            setting.SetIncludeAndroidEmulatorHost(true);
+
+            var authority = new Authority("10.0.2.2", 8080, false);
+            var variableContext = new VariableContext();
+            var exchangeContext = new ExchangeContext(authority, variableContext, setting, null!) {
+                RemoteHostIp = IPAddress.Parse("10.0.2.2"),
+                RemoteHostPort = 8080
+            };
+
+            // Create a minimal exchange for testing
+            var exchange = CreateTestExchange(8080);
+
+            var filter = new IsSelfFilter();
+
+            // Act
+            var result = filter.Apply(exchangeContext, authority, exchange, null);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsSelfFilter_WithAndroidEmulatorDisabled_DoesNotMatchEmulatorAddress()
+        {
+            // Arrange
+            var setting = FluxzySetting.CreateLocalRandomPort();
+            setting.SetIncludeAndroidEmulatorHost(false);
+
+            var authority = new Authority("10.0.2.2", 8080, false);
+            var variableContext = new VariableContext();
+            var exchangeContext = new ExchangeContext(authority, variableContext, setting, null!) {
+                RemoteHostIp = IPAddress.Parse("10.0.2.2"),
+                RemoteHostPort = 8080
+            };
+
+            // Create a minimal exchange for testing
+            var exchange = CreateTestExchange(8080);
+
+            var filter = new IsSelfFilter();
+
+            // Act
+            var result = filter.Apply(exchangeContext, authority, exchange, null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        private static Exchange CreateTestExchange(int port)
+        {
+            var authority = new Authority("10.0.2.2", port, false);
+            var requestHeader = "GET / HTTP/1.1\r\nHost: 10.0.2.2\r\n\r\n".AsMemory();
+            var exchange = new Exchange(IIdProvider.FromZero, authority, requestHeader, "HTTP/1.1", System.DateTime.UtcNow);
+            exchange.Metrics.DownStreamLocalPort = port;
+            return exchange;
+        }
+
         [Theory]
         [MemberData(nameof(AllValidHosts))]
         public async Task MakeMultipleSelfCall(string host)
