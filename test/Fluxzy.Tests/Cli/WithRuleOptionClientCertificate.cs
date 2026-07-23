d42a2872bbe50f05949716f61a89a44c337c1ed0
haga <haga.rakotoharivelo@gmail.com>
2026-07-23 07:49:30 +0200

    Drop client certificate test relying on certauth.cryptomix.com

--- test/Fluxzy.Tests/Cli/WithRuleOptionClientCertificate.cs
+++ test/Fluxzy.Tests/Cli/WithRuleOptionClientCertificate.cs
@@ -65,53 +65,5 @@ namespace Fluxzy.Tests.Cli
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal(expectedThumbPrint, thumbPrint, StringComparer.OrdinalIgnoreCase);
         }
-
-        [Theory]
-        [InlineData(false, false)]
-        [InlineData(true, false)]
-        [InlineData(false, true)]
-        [InlineData(true, true)]
-        public async Task Validate_ClientCertificate_Cryptomix(bool forceH11, bool useBouncyCastle)
-        {
-            var requestMessage =
-                new HttpRequestMessage(HttpMethod.Get, $"https://certauth.cryptomix.com/json/");
-
-            requestMessage.Headers.Add("X-Test-Header-256", "That value");
-            
-            File.WriteAllBytes("cc2.pfx", StorageContext.client_cert);
-            File.WriteAllBytes("cc.pfx", StorageContext.client_cert);
-
-            var yamlContent = $"""
-                              rules:
-                                - filter:
-                                    typeKind: AnyFilter
-                                  action :
-                                    typeKind: SetClientCertificateAction
-                                    clientCertificate:
-                                      pkcs12File: cc2.pfx
-                                      pkcs12Password: {CertificateContext.DefaultPassword}
-                                      retrieveMode: FromPkcs12
-                              """;
-
-            var yamlContentForceHttp11 = $"""
-                                         rules:
-                                           - filter:
-                                               typeKind: AnyFilter
-                                             action :
-                                               typeKind: SetClientCertificateAction
-                                               clientCertificate:
-                                                 pkcs12File: cc.pfx
-                                                 pkcs12Password: {CertificateContext.DefaultPassword}
-                                                 retrieveMode: FromPkcs12
-                                           - filter:
-                                               typeKind: AnyFilter
-                                             action :
-                                               typeKind: ForceHttp11Action
-                                         """;
-            
-            using var response = await Exec(forceH11 ? yamlContentForceHttp11 : yamlContent, 
-                requestMessage, useBouncyCastle: useBouncyCastle);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
     }
 }
